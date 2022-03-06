c858962b167614b8f4d99778ab05bca0992a40ec
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-03-06 22:49:12 +0100

    Welcome xcol!

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages, Extension
+
+#######################################
+# Prepare list of compiled extensions #
+#######################################
+
+extensions = []
+
+
+#########
+# Setup #
+#########
+
+setup(
+    name='xcol'
+    version='0.0.0',
+    description='Xsuite collimation package',
+    url='https://github.com/xsuite/xcol',
+    author='Giovanni Iadarola, A. Abramov, F. Van Der Veken',
+    packages=find_packages(),
+    ext_modules = extensions,
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.0',
+        'scipy',
+        'xobjects',
+        'xpart',
+        'xdeps',
+        'xtrack'
+        ]
+    )
