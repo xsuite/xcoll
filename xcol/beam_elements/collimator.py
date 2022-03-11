942bcf23196aafa92f645e6f764d2fdd2c2161bb
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-03-11 14:10:32 +0100

    A test

--- xcol/beam_elements/collimator.py
+++ xcol/beam_elements/collimator.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import xobjects as xo
 import xtrack as xt
 
@@ -16,6 +18,16 @@ class Collimator(xt.BeamElement):
         'cos_z': xo.Float64,
         'sin_z': xo.Float64,
     }
+
+    def __init__(self, angle=0, **kwargs):
+        anglerad = angle / 180 * np.pi
+        kwargs['cos_z']=np.cos(anglerad)
+        kwargs['sin_z']=np.sin(anglerad)
+        super().__init__(**kwargs)
+
+    @property
+    def angle(self):
+        return np.arctan2(self.sin_z, self.cos_z) * (180.0 / np.pi)
 
 Collimator.XoStruct.extra_sources = [
         _pkg_root.joinpath('beam_elements/collimator_src/collimator.h')]
