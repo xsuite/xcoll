0cdb4037cd0197f93adfe1c4278c2e8a3d5eb7a0
Frederik Van der Veken <frederik@cern.ch>
2022-03-22 03:10:49 +0100

    CollDB definition almost finished; only issues with None vs nan

--- xcoll/colldb.py
+++ xcoll/colldb.py
@@ -8,20 +8,205 @@ def load_SixTrack_colldb(filename, emitx, emity=None):
     
 class CollDB:
     def __init__(self, *, emitx, emity=None, sixtrack_file=None):
-        self.emitx = emitx
-        self.emity = emitx if emity is None else emity
-        self.gamma_rel = None
+        self._emitx = emitx
+        self._emity = emitx if emity is None else emity
+        self._betagamma_rel = None
         if sixtrack_file is not None:
             self.load_SixTrack(sixtrack_file)
         else:
             self._colldb = None
 
     def show(self):
-        return None
+        return pd.DataFrame({
+                'gap':             self.gap,
+                's_center':        self.s_center,
+                'angle':           self.angle,
+                'material':        self.material,
+                'offset':          self.offset,
+                'tilt':            self.tilt,
+                'stage':           self.stage,
+                'active_length':   self.active_length,
+                'collimator_type': self.collimator_type,
+            }, index=self.name)
     
     @property
     def name(self):
         return self._colldb.index.values
+
+
+    # TODO: - VALIDATION OF TYPES (e.g. material, stage)
+    #       - IMPLEMENTATION OF TILT
+    #       - CRYSTAL PROPERTIES: only valid if crystal == True (add mask to _set_property?)
+    #       - icoll
+    #       - show as __repr__
+    
+    # The CollDB class has the following fields (those marked
+    # with an * are set automatically and cannot be overwritten):
+    #   - name
+    #   - gap
+    #   - opening *
+    #   - beam_size *
+    #   - s_center *
+    #   - angle
+    #   - material
+    #   - offset
+    #   - tilt
+    #   - stage
+    #   - onesided
+    #   - active_length
+    #   - inactive_front
+    #   - inactive_back
+    #   - total_length *
+    #   - collimator_type *
+    #   - betx
+    #   - bety
+    #   - x
+    #   - px
+    #   - y
+    #   - py
+    #   - gamma_rel
+    #   - emit
+    #
+    # Additionally, for crystals the following fields are added:
+    #   - crystal
+    #   - bend
+    #   - xdim
+    #   - ydim
+    #   - miscut
+    #   - thick
+    
+    @property
+    def angle(self):
+        return self._colldb['angle']
+
+    @angle.setter
+    def angle(self, angle):
+        self._set_property('angle', angle)
+
+    @property
+    def material(self):
+        return self._colldb['material']
+
+    @material.setter
+    def material(self, material):
+        self._set_property('material', material)
+
+    @property
+    def offset(self):
+        return self._colldb['offset']
+
+    @offset.setter
+    def offset(self, offset):
+        self._set_property('offset', offset)
+
+    @property
+    def tilt(self):
+        tilts = np.array([self._colldb.tilt_L.values,self._colldb.tilt_R.values])
+        return pd.Series([ L if L == R else [L,R] for L, R in tilts.T ], index=self._colldb.index, dtype=object)
+
+    @tilt.setter
+    def tilt(self, tilts):
+        self._set_property_LR('tilt', tilts)
+
+    @property
+    def stage(self):
+        return self._colldb['stage']
+
+    @stage.setter
+    def stage(self, stage):
+        self._set_property('stage', stage)
+
+    @property
+    def parking(self):
+        return self._colldb['parking']
+
+    @parking.setter
+    def parking(self, parking):
+        self._set_property('parking', parking)
+
+#     @property
+#     def crystal(self):
+#         return self._colldb['crystal']
+
+#     @crystal.setter
+#     def crystal(self, crystal):
+#         self._set_property('crystal', crystal)
+
+#     @property
+#     def bend(self):
+#         return self._colldb['bend']
+
+#     @bend.setter
+#     def bend(self, bend):
+#         self._set_property('bend', bend)
+
+#     @property
+#     def xdim(self):
+#         return self._colldb['xdim']
+
+#     @xdim.setter
+#     def xdim(self, xdim):
+#         self._set_property('xdim', xdim)
+
+#     @property
+#     def ydim(self):
+#         return self._colldb['ydim']
+
+#     @ydim.setter
+#     def ydim(self, ydim):
+#         self._set_property('ydim', ydim)
+
+#     @property
+#     def miscut(self):
+#         return self._colldb['miscut']
+
+#     @miscut.setter
+#     def miscut(self, miscut):
+#         self._set_property('miscut', miscut)
+
+#     @property
+#     def thick(self):
+#         return self._colldb['thick']
+
+#     @thick.setter
+#     def thick(self, thick):
+#         self._set_property('thick', thick)
+
+    @property
+    def s_center(self):
+        return self._colldb['s_center']
+
+    @property
+    def collimator_type(self):
+        return self._colldb['collimator_type']
+    
+    @property
+    def active_length(self):
+        return self._colldb['active_length']
+
+    @active_length.setter
+    def active_length(self, lengths):
+        self._set_property('active_length', lengths)
+
+    @property
+    def inactive_front(self):
+        return self._colldb['inactive_front']
+    
+    @inactive_front.setter
+    def inactive_front(self, lengths):
+        self._set_property('inactive_front', lengths)
+
+    @property
+    def inactive_back(self):
+        return self._colldb['inactive_back']
+    
+    @inactive_back.setter
+    def inactive_back(self, lengths):
+        self._set_property('inactive_back', lengths)
+
+    @property
+    def total_length(self):
+        return self._colldb['active_length'] +  self._colldb['inactive_front'] + self._colldb['inactive_back']
     
     @property
     def gap(self):
@@ -30,6 +215,7 @@ class CollDB:
     
     @gap.setter
     def gap(self, gaps):
+        df = self._colldb
         correct_format = False
         # The variable gaps is a Series or a list
         if isinstance(gaps, pd.Series) or isinstance(gaps, list) or isinstance(gaps, np.ndarray):
@@ -42,12 +228,12 @@ class CollDB:
                 gaps = dict(zip(self.name, gaps))
             # All gaps are single values: use pandas-style assignment
             else:
-                mask_L = self._colldb.onesided.isin(['both','left'])
-                mask_R = self._colldb.onesided.isin(['both','right'])
-                self._colldb.loc[mask_L, 'gap_L'] = gaps[mask_L]
-                self._colldb.loc[~mask_L, 'gap_L'] = None
-                self._colldb.loc[mask_R, 'gap_R'] = gaps[mask_R]
-                self._colldb.loc[~mask_R, 'gap_R'] = None
+                mask_L = df.onesided.isin(['both','left'])
+                mask_R = df.onesided.isin(['both','right'])
+                df.loc[mask_L, 'gap_L'] = gaps[mask_L]
+                df.loc[~mask_L, 'gap_L'] = None
+                df.loc[mask_R, 'gap_R'] = gaps[mask_R]
+                df.loc[~mask_R, 'gap_R'] = None
         
         # The variable gaps is a dictionary
         if isinstance(gaps, dict):
@@ -55,7 +241,7 @@ class CollDB:
             for name, gap in gaps.items():
                 if name not in self.name:
                     raise ValueError(f"Collimator {name} not found in CollDB!")
-                side = self._colldb.onesided[name]
+                side = df.onesided[name]
                 if hasattr(gap, '__iter__'):
                     if isinstance(gap, str):
                         raise ValueError("The gap setting has to be a number!")
@@ -77,19 +263,23 @@ class CollDB:
                 else:
                     gap_L = gap if side in ['both','left'] else None
                     gap_R = gap if side in ['both','right'] else None
-                self._colldb.loc[name, 'gap_L'] = gap_L
-                self._colldb.loc[name, 'gap_R'] = gap_R
+                df.loc[name, 'gap_L'] = gap_L
+                df.loc[name, 'gap_R'] = gap_R
 
         if not correct_format:
             raise ValueError("Variable `gaps` needs to be a pandas Series, dict, numpy array, or list!")
         
-        self._colldb.gap_L = self._colldb.gap_L.astype('object', copy=False)
-        self._colldb.gap_R = self._colldb.gap_R.astype('object', copy=False)
+        df.gap_L = df.gap_L.astype('object', copy=False)
+        df.gap_R = df.gap_R.astype('object', copy=False)
             
         # Recompute openings
         self._compute_opening
 
-
+    @property
+    def opening(self):
+        openings = np.array([self._colldb.opening_L.values,self._colldb.opening_R.values])
+        return pd.Series([ L if L == R else [L,R] for L, R in openings.T ], index=self._colldb.index, dtype=object)
+    
     @property
     def onesided(self):
         return self._colldb.onesided
@@ -97,34 +287,118 @@ class CollDB:
     @onesided.setter
     def onesided(self, sides):
         self._set_property('onesided', sides)
+        df = self._colldb
         df['onesided'] = [ 'both' if s==0 else s for s in df['onesided'] ]    
         df['onesided'] = [ 'left' if s==1 else s for s in df['onesided'] ]
         df['onesided'] = [ 'right' if s==2 else s for s in df['onesided'] ]
         self.gap = self.gap
 
+    @property
+    def gamma_rel(self):
+        return np.sqrt(self._beta_gamma_rel**2+1)
+
+    @gamma_rel.setter
+    def gamma_rel(self, gamma_rel):
+        self._beta_gamma_rel = np.sqrt(gamma_rel**2-1)
+        self._compute_opening()
 
     @property
-    def sigma(self):
-        sigma = np.array([self._colldb.sigmax.values,self._colldb.sigmay.values])
-        return pd.Series(sigma.T, index=self._colldb.index, dtype=object) 
+    def emit(self):
+        return [self._emitx, self._emity]
+    
+    @emit.setter
+    def emit(self, emit):
+        if hasattr(emit, '__iter__'):
+            if isinstance(emit, str):
+                raise ValueError(f"The `emit` setting has to be a number!")
+            elif len(emit) == 2:
+                self._emitx = emit[0]
+                self._emity = emit[1]
+            elif len(val) == 1:
+                self._emitx = emit[0]
+                self._emity = emit[0]
+            else:
+                raise ValueError(f"The `emit` setting must have one or two values (for emitx and emity)!")
+        else:
+            self._emitx = emit
+            self._emity = emit
+        self._compute_opening()
 
     @property
-    def active_length(self):
-        return self._colldb['active_length']
+    def betx(self):
+        return self._colldb['betx']
     
-    @active_length.setter
-    def active_length(self, lengths):
-        self._set_property('active_length', lengths)
+    @betx.setter
+    def betx(self, betx):
+        self._set_property('betx', betx)
+        self._compute_opening()
 
-            
-    
-    
     @property
-    def total_length(self):
-        return self._colldb['active_length'] +  self._colldb['inactive_front'] + self._colldb['inactive_back']
+    def bety(self):
+        return self._colldb['bety']
     
+    @bety.setter
+    def bety(self, bety):
+        self._set_property('bety', bety)
+        self._compute_opening()
+
+    @property
+    def x(self):
+        return self._colldb['x']
     
+    @x.setter
+    def x(self, x):
+        self._set_property('x', x)
+
+    @property
+    def px(self):
+        return self._colldb['px']
     
+    @px.setter
+    def px(self, px):
+        self._set_property('px', px)
+
+    @property
+    def y(self):
+        return self._colldb['y']
+    
+    @y.setter
+    def y(self, y):
+        self._set_property('y', y)
+
+    @property
+    def py(self):
+        return self._colldb['py']
+    
+    @py.setter
+    def py(self, py):
+        self._set_property('py', py)
+
+    @property
+    def beam_size(self):
+        df = self._colldb
+        return np.sqrt(
+                (df['sigmax']*np.cos(np.float_(df['angle'].values)*np.pi/180))**2
+                + (df['sigmay']*np.sin(np.float_(df['angle'].values)*np.pi/180))**2
+            )
+
+    def _compute_opening(self):
+        df = self._colldb
+        incomplete = np.any([ np.any([ x is None for x in df[opt] ]) for opt in ['betx', 'bety'] ])
+        if self._beta_gamma_rel is None or incomplete:
+            pass
+        else:
+            df['sigmax'] = np.sqrt(df['betx']*self._emitx/self._beta_gamma_rel)
+            df['sigmay'] = np.sqrt(df['bety']*self._emity/self._beta_gamma_rel)
+            df['opening_L'] = df['parking'] if df['gap_L'] is None else np.minimum(df['gap_L']*self.beam_size,df['parking'])
+            df['opening_R'] = df['parking'] if df['gap_R'] is None else np.minimum(df['gap_R']*self.beam_size,df['parking'])
+
+
+
+    # ---------------------------------------
+    # ------ Property setter functions ------
+    # ---------------------------------------
+
     def _set_property(self, prop, vals):
         df = self._colldb
         if isinstance(vals, dict):
@@ -139,28 +413,74 @@ class CollDB:
             df[prop] = vals
         else:
             raise ValueError(f"Variable `{prop}` needs to be a pandas Series, dict, numpy array, or list!")
-    
-    
-    def _compute_opening(self):
-        incomplete = np.any([ np.any([ x is None for x in colldb[opt] ]) for opt in ['betx', 'bety'] ])
-        if self.gamma_rel is None or incomplete:
-            pass
-        else:
-            self._colldb['sigmax'] = np.sqrt(self._colldb['betx']*self.emitx/self.gamma_rel)
-            self._colldb['sigmay'] = np.sqrt(self._colldb['bety']*self.emity/self.gamma_rel)
 
-            
+
+    def _set_property_LR(self, prop, vals):
+        df = self._colldb
+        correct_format = False
+        # The variable vals is a Series or a list
+        if isinstance(vals, pd.Series) or isinstance(vals, list) or isinstance(vals, np.ndarray):
+            correct_format = True
+            if len(vals) != len(self.name):
+                raise ValueError(f"The variable `{prop}` has a different length than the number of collimators in the CollDB. "
+                                + "Use a dictionary instead.")
+            # Some of the vals are list (e.g. two different values for both gaps): loop over vals as dict
+            if any(hasattr(val, '__iter__') for val in vals):
+                vals = dict(zip(self.name, vals))
+            # All gaps are single values: use pandas-style assignment
+            else:
+                df[prop + "_L"] = vals
+                df[prop + "_R"] = vals
+        
+        # The variable gaps is a dictionary
+        if isinstance(vals, dict):
+            correct_format = True
+            for name, val in vals.items():
+                if name not in self.name:
+                    raise ValueError(f"Collimator {name} not found in CollDB!")
+                if hasattr(val, '__iter__'):
+                    if isinstance(val, str):
+                        raise ValueError(f"The `{prop}` setting has to be a number!")
+                    elif len(val) == 2:
+                        val_L = val[0]
+                        val_R = val[1]
+                    elif len(val) == 1:
+                        val_L = val[0]
+                        val_R = val[0]
+                    else:
+                        raise ValueError(f"The `{prop}` setting must have one or two values (for the left and the right jaw)!")
+                else:
+                    val_L = val
+                    val_R = val
+                df.loc[name, prop + "_L"] = val_L
+                df.loc[name, prop + "_R"] = val_R
+
+        if not correct_format:
+            raise ValueError("Variable `{prop}` needs to be a pandas Series, dict, numpy array, or list!")
+
+        df[prop + "_L"] = df[prop + "_L"].astype('object', copy=False)
+        df[prop + "_R"] = df[prop + "_R"].astype('object', copy=False)
+                
             
     def _initialise_None(self):
-        fields = {'s_center':None, 'gap_L': None, 'gap_R': None, 'opening_L': None, 'opening_R': None, 'active_length': 0}
-        fields.update({'angle': 0, 'onesided': 0, 'material': None, 'stage': None, 'type':None, 'tilt_L': 0, 'tilt_R': 0})
-        fields.update({'offset': 0, 'inactive_front': 0, 'inactive_back': 0, 'sigmax': None, 'sigmay': None})
+        fields = {'s_center':None, 'gap_L': None, 'gap_R': None, 'opening_L': None, 'opening_R': None, 'angle': 0, 'offset': 0}
+        fields.update({'parking': 0.025})
+        fields.update({'onesided': 'both', 'material': None, 'stage': None, 'collimator_type': None, 'tilt_L': 0, 'tilt_R': 0})
+        fields.update({'active_length': 0, 'inactive_front': 0, 'inactive_back': 0, 'sigmax': None, 'sigmay': None})
         fields.update({'crystal': None, 'bend': None, 'xdim': 0, 'ydim': 0, 'miscut': 0, 'thick': 0})
         fields.update({'betx': None, 'bety': None, 'x': None, 'px': None, 'y': None, 'py': None})
         for f, val in fields.items():
             if f not in self._colldb.columns:
                 self._colldb[f] = val
-            
+
+
+
+
+
+    # -------------------------------
+    # ------ Loading functions ------
+    # -------------------------------
+
     def load_SixTrack(self,filename):
         with open(filename, 'r') as infile:
             coll_data_string = ''
@@ -195,19 +515,9 @@ class CollDB:
         df.insert(5,'stage', df['opening'].apply(lambda s: family_types.get(s, 'UNKNOWN')))   
         
         sides = df['name'].apply(lambda s: onesided.get(s, 0))
-#         df['onesided'] = [ 'left' if s==1 else s for s in df['onesided'] ]
-#         df['onesided'] = [ 'right' if s==2 else s for s in df['onesided'] ]
-#         df['tilt_left'] = 0
-#         df['tilt_right'] = 0
-#         df['inactive_front'] = 0
-#         df['inactive_back'] = 0
-#         df['total_length'] = df['length']
-#         df['crystal'] = None
-#         df['bend'] = None
-#         df['xdim'] = None
-#         df['ydim'] = None
-#         df['miscut'] = None
-#         df['thick'] = None
+        gaps = df.astype('object', copy=False)['opening'].apply(
+            lambda s: None if float(family_settings.get(s, s)) >= 900 else float(family_settings.get(s, s))
+        ).astype('object', copy=False)
 
         df['name'] = df['name'].str.lower() # Make the names lowercase for easy processing
         df.rename(columns={'length':'active_length'}, inplace=True)
@@ -215,8 +525,9 @@ class CollDB:
         self._colldb = df.drop('opening', 1)
         
         self._initialise_None()
-        self.gap = df['opening'].apply(lambda s: float(family_settings.get(s, s)))
-        self.onesided = sides
+        print([ type(gap) for gap in gaps.values])
+        self.gap = gaps.values
+        self.onesided = sides.values
 
         # Check if collimator active
         # Check if gap is list (assymetric jaws)
--- xcoll/manager.py
+++ xcoll/manager.py
@@ -6,37 +6,29 @@ from .beam_elements import Collimator
 from .colldb import CollDB
 
 class CollimatorManager:
-    def __init__(self, *, line, coll_db_txt_file, emitx, emity):
-
-        colldb = _load_colldb(coll_db_txt_file)
-        colldb.insert(1,'s_center', None)
-        colldb.insert(1,'opening', None)
-        colldb['type'] = None
-        
+    def __init__(self, *, line, colldb: CollDB):        
         self.colldb = colldb
         self.line = line
-        self.emitx = emitx
-        self.emity = emity
 
     @property
     def collimator_names(self):
-        return list(self.colldb.index.values)
+        return list(self.colldb.name)
 
 
     def install_black_absorbers(self, names=None, verbose=False):
         line = self.line
-        colldb = self.colldb
+        df = self.colldb._colldb
         if names is None:
-            names = colldb.index.values
-        mask = colldb.index.isin(names)
+            names = self.collimator_names
+        mask = df.index.isin(names)
         for name in names:
             if name not in line.element_names:
                 raise Exception(f"Collimator {name} not found in line!")
         if line.tracker is not None:
             raise Exception("Tracker already built! Please install collimators before building tracker!")
 
-        colldb.loc[mask,'s_center'] = line.get_s_position(names)
-        colldb.loc[mask,'type'] = 'BlackAbsorber'
+        df.loc[mask,'s_center'] = line.get_s_position(names)
+        df.loc[mask,'collimator_type'] = 'BlackAbsorber'
 
         for name in names:
             if isinstance(line[name], Collimator):
@@ -47,7 +39,7 @@ class CollimatorManager:
             else:
                 if verbose:
                     print(f"Installing {name}")
-                thiscoll = colldb.loc[name]
+                thiscoll = df.loc[name]
                 newcoll = Collimator(
                         inactive_front=thiscoll['inactive_front'],
                         inactive_back=thiscoll['inactive_back'],
@@ -62,9 +54,9 @@ class CollimatorManager:
 
     def install_k2_collimators(self, names=None, verbose=False):
         line = self.line
-        colldb = self.colldb
+        df = self.colldb._colldb
         if names is None:
-            names = colldb.index.values
+            names = self.collimator_names
         mask = colldb.index.isin(names)
         for name in names:
             if name not in line.element_names:
@@ -113,16 +105,14 @@ class CollimatorManager:
             raise Exception("Please build tracker before calling this method!")
         opt_funcs = ['betx', 'bety', 'x', 'px', 'y', 'py'] 
 
-        colldb = self.colldb
-        incomplete = np.any([ np.any([ x is None for x in colldb[opt] ]) for opt in opt_funcs + ['sigmax', 'sigmay'] ])
+        df = self.colldb._colldb
+        incomplete = np.any([ np.any([ x is None for x in df[opt] ]) for opt in opt_funcs ])
         if recompute or incomplete:
             tracker = line.tracker
-            tw = tracker.twiss(at_s=colldb['s_center'])
+            tw = tracker.twiss(at_s=df['s_center'])
             for opt in opt_funcs:
-                colldb[opt] = tw[opt]
-            beta0_gamma0 = tracker.particle_ref._xobject.beta0[0] * tracker.particle_ref._xobject.gamma0[0]
-            colldb['sigmax'] = np.sqrt(colldb['betx']*self.emitx/beta0_gamma0)
-            colldb['sigmay'] = np.sqrt(colldb['bety']*self.emity/beta0_gamma0)
+                df[opt] = tw[opt]
+            self.colldb.gamma_rel = tracker.particle_ref._xobject.beta0[0] * tracker.particle_ref._xobject.gamma0[0]
 
 
     def set_openings(self, gaps={}, recompute_optics=False):
@@ -130,35 +120,21 @@ class CollimatorManager:
         if line is None or line.tracker is None:
             raise Exception("Please build tracker before calling this method!")
         colldb = self.colldb
-        if any([ x is None for x in colldb.type ]):
+        if any([ x is None for x in colldb.collimator_type ]):
             raise ValueError("Some collimators have not yet been installed. "
                              + "Please install all collimators before setting the openings.")
-        for name, gap in gaps.items():
-            if hasattr(gap, '__iter__'):
-                if isinstance(gap, str):
-                    raise ValueError("The gap setting has to be a number!")
-                elif len(gap) > 2:
-                    raise ValueError("The gap setting can maximally have two values (for the left and the right jaw)!")
-                elif len(gap) == 1:
-                    colldb.loc[name, 'gap'] = gap
-                elif len(gap) == 2:
-                    colldb.loc[name, 'gap'] = gap
-                colldb.loc[name, 'gap'] = gap
+        colldb.gap = gaps
             
         # Compute halfgap
         self._compute_optics(recompute=recompute_optics)
-        colldb['opening'] = colldb['gap'] * np.sqrt(
-            (colldb['sigmax']*np.cos(np.float_(colldb['angle'].values)*np.pi/180))**2
-            + (colldb['sigmay']*np.sin(np.float_(colldb['angle'].values)*np.pi/180))**2
-        )
 
         # Configure collimators
-        for name in colldb.index:
+        for name in self.collimator_names:
             if isinstance(line[name], Collimator):
-                line[name].dx = colldb['x'][name]
-                line[name].dy = colldb['y'][name]
-                line[name].jaw_R = -colldb['opening'][name] + colldb['offset'][name]
-                line[name].jaw_L = colldb['opening'][name] + colldb['offset'][name]
+                line[name].dx = colldb.x[name]
+                line[name].dy = colldb.y[name]
+                line[name].jaw_R = -colldb._colldb['opening_L'][name] + colldb.offset[name]
+                line[name].jaw_L = colldb._colldb['opening_R'][name] + colldb.offset[name]
             elif isinstance(line[name], K2Collimator):
                 pass
             else:
