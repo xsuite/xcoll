e103c3c6a6b1f1fdf0d9abed763328b8c95985bc
Frederik F. Van der Veken <frederikvanderveken@gmail.com>
2022-05-03 04:04:03 +0200

    Updated version number to v0.1.0.

--- xcoll/__init__.py
+++ xcoll/__init__.py
@@ -4,4 +4,4 @@ from .beam_elements import BlackAbsorber, K2Collimator, K2Engine
 from .manager import CollimatorManager
 from .colldb import CollDB, load_SixTrack_colldb
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
