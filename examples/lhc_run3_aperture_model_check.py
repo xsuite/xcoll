52d10ef1ef18709c2f3d79f56d82aad23370b2ee
giadarol <giovanni.iadarola@cern.ch>
2022-09-08 22:35:45 +0200

    Patched aperture in example

--- examples/lhc_run3_aperture_model_check.py
+++ examples/lhc_run3_aperture_model_check.py
@@ -31,8 +31,20 @@ line['acsca.b5r4.b1'].frequency = 400e6
 line['acsca.c5r4.b1'].frequency = 400e6
 line['acsca.d5r4.b1'].frequency = 400e6
 
-df = line.check_aperture()
+df_imported = line.check_aperture()
 
+# Patch aperture model
+aperture_for_octupoles = line['mo.22r1.b1_mken_aper'].copy()
+aperture_for_mkwa = line['mqwa.e5l3.b1_mken_aper'].copy()
 
+for nn in ['mo.28r3.b1', 'mo.32r3.b1']:
+    line.insert_element(index=nn, element=aperture_for_octupoles,
+                        name=nn+'_aper_patch')
 
+for nn in ['mqwa.f5l7.b1..1', 'mqwa.f5l7.b1..2', 'mqwa.f5l7.b1..3',
+           'mqwa.f5l7.b1..4', 'mqwa.f5r7.b1..1', 'mqwa.f5r7.b1..2',
+           'mqwa.f5r7.b1..3', 'mqwa.f5r7.b1..4']:
+    line.insert_element(index=nn, element=aperture_for_mkwa,
+                        name=nn+'_aper_patch')
 
+df_patched = line.check_aperture()
