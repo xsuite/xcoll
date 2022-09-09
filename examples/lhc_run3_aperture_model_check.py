becd173c6ad6a1dc8ae6d7dabd649680ada9c8d2
giadarol <giovanni.iadarola@cern.ch>
2022-09-09 17:36:53 +0200

    Patched all elments with issues

--- examples/lhc_run3_aperture_model_check.py
+++ examples/lhc_run3_aperture_model_check.py
@@ -36,20 +36,34 @@ df_imported = line.check_aperture()
 
 # Patch aperture model
 aperture_for_octupoles = line['mo.22r1.b1_mken_aper'].copy()
-aperture_for_mqwa = line['mqwa.e5l3.b1_mken_aper'].copy()
 
 for nn in ['mo.28r3.b1', 'mo.32r3.b1']:
     line.insert_element(index=nn, element=aperture_for_octupoles,
                         name=nn+'_aper_patch')
 
+aperture_for_mqwa = line['mqwa.e5l3.b1_mken_aper'].copy()
 for nn in ['mqwa.f5l7.b1..1', 'mqwa.f5l7.b1..2', 'mqwa.f5l7.b1..3',
            'mqwa.f5l7.b1..4', 'mqwa.f5r7.b1..1', 'mqwa.f5r7.b1..2',
            'mqwa.f5r7.b1..3', 'mqwa.f5r7.b1..4']:
     line.insert_element(index=nn, element=aperture_for_mqwa,
                         name=nn+'_aper_patch')
 
-df_patched = line.check_aperture()
+aperture_for_tdis = line['tdis.4l2.a.b1_aper'].copy()
+for nn in ['tdisa.a4l2.b1', 'tdisb.a4l2.b1', 'tdisc.a4l2.b1']:
+    line.insert_element(index=nn, element=aperture_for_tdis,
+                        name=nn+'_aper')
 
+aperture_for_tcld = xt.LimitEllipse(a=4e-2, b=4e-2)
+for nn in ['tcld.a11r2.b1']:
+    line.insert_element(index=nn, element=aperture_for_tdis,
+                            name=nn+'_aper')
+
+aperture_for_tcspm = line['tcspm.6r7.a.b1_aper'].copy()
+for nn in ['tcspm.b4l7.b1', 'tcspm.e5r7.b1', 'tcspm.6r7.b1']:
+    line.insert_element(index=nn, element=aperture_for_tcspm,
+                        name=nn+'_aper')
+
+df_patched = line.check_aperture()
 
 # Initialise collmanager,on the specified buffer
 coll_manager = xc.CollimatorManager(
