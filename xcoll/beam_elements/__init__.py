487fd36a95627011e81f750b3f9518b38de6d7b7
Frederik Van der Veken <frederik@cern.ch>
2022-10-16 07:41:12 +0200

    made geant4 class

--- xcoll/beam_elements/__init__.py
+++ xcoll/beam_elements/__init__.py
@@ -1,2 +1,3 @@
 from .collimators import BlackAbsorber
 from .k2collimator import K2Collimator, K2Engine
+from .geant4_collimator import Geant4Collimator
---
+++ xcoll/beam_elements/geant4_collimator.py
@@ -0,0 +1,22 @@
+import numpy as np
+# from .k2.materials import Material, Crystal
+from .collimators import BaseCollimator
+
+from xtrack import BeamInteraction
+
+class Geant4Collimator(BaseCollimator, BeamInteraction):
+
+    iscollective = True
+
+    def __init__(self, **kwargs):
+        super().__init__(self, inactive_front=10)
+        # We skip the BeamInteraction initialisation (to avoid issues with setting the length)
+        # and manually set the necessary flags
+        self.name = 'TEST'
+        self.interaction_process = None
+        self.isthick = True
+
+
+    def track(self, particles):
+        BeamInteraction.track(self, particles)
+    
