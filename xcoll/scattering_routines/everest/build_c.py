03620c7be561b1165b7bea0eae8083e715328282
Despina Demetriadou <despina.demetriadou@cern.ch>
2022-11-24 14:21:24 +0100

    testing_random_generator notebook available for comparison of Everest and K2 random generators.

--- xcoll/scattering_routines/everest/build_c.py
+++ xcoll/scattering_routines/everest/build_c.py
@@ -6,10 +6,12 @@ ffibuilder = FFI()
 # ffibuilder.cdef("float iterat(float a, float b, float dh, float s);")
 ffibuilder.cdef("double soln3(double a, double b, double dh, double smax, double s);")
 ffibuilder.cdef("static double get_random(void);")
+ffibuilder.cdef("static double get_random_normal(void);")
 ffibuilder.cdef("static double get_random_gauss(void);")
-ffibuilder.cdef("static double get_random_ruth(int n);")
+ffibuilder.cdef("static double get_random_ruth(float z, float emr, double lower_val, double upper_val, unsigned int n);")
 ffibuilder.cdef("static void set_random_seed(unsigned int seed);")
 ffibuilder.cdef("static unsigned int get_random_seed();")
+ffibuilder.cdef("static unsigned int get_n_sampled();")
 
 ffibuilder.set_source("_everest",  # name of the output C extension
 """
--- xcoll/scattering_routines/everest/random.h
+++ xcoll/scattering_routines/everest/random.h
@@ -3,6 +3,7 @@
 #include <time.h>
 
 unsigned int this_seed;
+unsigned int n_sampled = 0;
 
 static void set_random_seed(unsigned int seed){
     /* Intializes random number generator */
@@ -15,16 +16,36 @@ static void set_random_seed(unsigned int seed){
     srand(this_seed);
 }
 
-// This wrong if not manually set first: it will return 0, but not sure what the default seed is..
+// This is wrong if not manually set first: it will return 0, but not sure what the default seed is..
 static unsigned int get_random_seed(){
     return this_seed;
+}
+
+static unsigned int get_n_sampled(){
+    return n_sampled;
 }
 
 // Generate a random uniform value in [0,1]
 // Note that Ranlux (in old K2) generated randoms in ]0,1[
 static double get_random(void){
-  double x = (double)rand() / RAND_MAX;
+  n_sampled = n_sampled + 1;
+  double x = (double)rand()/RAND_MAX;
   return x;
+}
+
+
+
+
+
+
+
+static double get_random_normal(void) {
+    double u = ((double) rand() / (RAND_MAX)) * 2 - 1;
+    double v = ((double) rand() / (RAND_MAX)) * 2 - 1;
+    double r = u * u + v * v;
+    if (r == 0 || r > 1) return get_random_normal();
+    double c = sqrt(-2 * log(r) / r);
+    return u * c;
 }
 
 // Generate a random value weighted within the normal (Gaussian) distribution
@@ -35,16 +56,15 @@ static double get_random_gauss(void){
   return z;
 }
 
+
 // PDF of Rutherford distribution
-static double PDF(float t){
+static double ruth_PDF(float t, float z, float emr){
 
     // Constants
-    double z = 29;
     double c = 0.8561e3;
     double N = 2.607e-5;
-    double e = 0.302;
     double A = N*pow(z,2);
-    double B = c*pow(e,2);
+    double B = c*pow(emr,2);
 
     // CDF from Rutherford PDF
     double F = (A/pow(t,2))*(exp(-B*t));
@@ -54,44 +74,40 @@ static double PDF(float t){
 }
 
 // CDF of Rutherford distribution
-static double CDF(float t, float t0){
+static double ruth_CDF(float t, float z, float emr, float t0){
 
     // Constants
-    double z = 29;
     double c = 0.8561e-3;
     double N = 2.607e-5;
-    double e = 0.302;
     double A = N*pow(z,2);
-    double B = c*pow(e,2);
+    double B = c*pow(emr,2);
 
     // CDF from Rutherford PDF
-    double F = -A*B*Exponential_Integral_Ei(-B*t) - t*PDF(t) + A*B*Exponential_Integral_Ei(-B*t0) + t0*PDF(t0);
+    double F = -A*B*Exponential_Integral_Ei(-B*t) - t*ruth_PDF(t,z,emr) + A*B*Exponential_Integral_Ei(-B*t0) +       t0*ruth_PDF(t0,z,emr);
 
     return F;
         
 }
 
 // Generate a random value weighted with a Rutherford distribution
-static double get_random_ruth(int n){
+static double get_random_ruth(float z, float emr, double lower_val, double upper_val, unsigned int n){
 
-    // range of rutherford random
-    double lower_CDF = 0.0009982;
-    double upper_CDF = 0.01;
+//     unsigned int n = 20; //iterations
 
     // corresponding range in uniform
     // double lower_uni = 0;
-    double upper_uni = CDF(upper_CDF, lower_CDF);
+    double upper_uni = ruth_CDF(upper_val,z,emr,lower_val);
 
     // sample a random uniform
     double t = upper_uni * get_random() ;
     
     // initial estimate the lower border
-    double x = lower_CDF;
+    double x = lower_val;
 
     // solve CDF(x) == t for x
     unsigned short i = 1;
     while(i <= n) {
-        x = x - (CDF(x, lower_CDF)-t)/PDF(x);
+        x = x - (ruth_CDF(x,z,emr,lower_val)-t)/ruth_PDF(x,z,emr);
         i++;
     }
 
