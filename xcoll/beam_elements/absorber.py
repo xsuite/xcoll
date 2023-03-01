b260d484373c57f3fe67069352eecc9803aeb9b2
Frederik Van der Veken <frederik@cern.ch>
2023-03-01 17:58:12 +0100

    Added backtrack element to BlackAbsorber.

--- xcoll/beam_elements/absorber.py
+++ xcoll/beam_elements/absorber.py
@@ -33,3 +33,6 @@ class BlackAbsorber(BaseCollimator):
             kwargs.setdefault('_tracking', True)
         super().__init__(**kwargs)
 
+    def get_backtrack_element(self, _context=None, _buffer=None, _offset=None):
+        return InvalidCollimator(length=-self.length, _context=_context, _buffer=_buffer, _offset=_offset)
+
