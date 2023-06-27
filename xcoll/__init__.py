6b5471ab143a51e074d3cae2c65e3e6d6398d12a
Frederik Van der Veken <frederik@cern.ch>
2023-06-27 19:46:58 +0200

    Initialising Rutherford parameters before tracking.

--- xcoll/__init__.py
+++ xcoll/__init__.py
@@ -1,8 +1,7 @@
 from .general import _pkg_root
 
 from .beam_elements import BaseCollimator, BlackAbsorber, EverestCollimator, EverestCrystal
-from .scattering_routines.everest import materials
-from .scattering_routines.everest.materials import GeneralMaterial, Material, CrystalMaterial
+from .scattering_routines.everest import materials, Material, CrystalMaterial
 from .manager import CollimatorManager
 from .colldb import CollimatorDatabase, load_SixTrack_colldb
 
