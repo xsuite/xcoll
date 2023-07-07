c5919990ac766871f304cf39250786c7d7162f95
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2023-07-07 18:11:06 +0200

    Add the Geant4 colliamtor to the init and to the list of available collimators

--- xcoll/beam_elements/__init__.py
+++ xcoll/beam_elements/__init__.py
@@ -1,5 +1,6 @@
 from .base_collimator import BaseCollimator
 from .absorber import BlackAbsorber
 from .everest_collimator import EverestCollimator, EverestCrystal
+from .geant4_collimator import Geant4Collimator
 
-_all_collimator_types = { BlackAbsorber, EverestCollimator, EverestCrystal }
+_all_collimator_types = { BlackAbsorber, EverestCollimator, EverestCrystal, Geant4Collimator}
