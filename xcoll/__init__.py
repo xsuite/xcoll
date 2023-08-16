4c6c82ef6e54764a1d9755dc8d298a3d85b19e4a
Frederik Van der Veken <frederik@cern.ch>
2023-08-16 15:24:36 +0200

    Updated version number to v0.2.5.

--- xcoll/__init__.py
+++ xcoll/__init__.py
@@ -5,4 +5,4 @@ from .scattering_routines.everest import materials, Material, CrystalMaterial
 from .manager import CollimatorManager
 from .colldb import CollimatorDatabase, load_SixTrack_colldb
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
