1ed1232d360e111554f129a052173dd8cf16a7ed
Frederik Van der Veken <frederik@cern.ch>
2023-08-28 18:42:07 +0200

    Updated off-momentum loss map example.

--- examples/lhc_run3_offmomentum_lossmap.py
+++ examples/lhc_run3_offmomentum_lossmap.py
@@ -7,16 +7,14 @@ import xcoll as xc
 
 
 # On a modern CPU, we get ~5000 particle*turns/s
-# So this script should take around 45 minutes
+# This script typically takes around 1 hour
 beam      = 1
 plane    = 'DPpos'
 
-num_particles  = 2000
+num_particles  = 500
 sweep          = 300
 sweep          = -abs(sweep) if plane == 'DPpos' else abs(sweep)
-pretrack_turns = 500
 num_turns      = int(20*abs(sweep))
-at_element     = 'ip3'
 engine         = 'everest'
 
 path_in  = xc._pkg_root.parent / 'examples'
@@ -59,41 +57,15 @@ coll_manager.build_tracker()
 coll_manager.set_openings()
 
 
-# Generate initial halo distribution in IP3  (better matching)
-# This creates amplitudes on a circle with R = [4, 6], e.g.:
-# plt.scatter(np.sqrt(x_norm**2 + px_norm**2), np.sqrt(y_norm**2 + py_norm**2))
-x_norm, px_norm, _, _ = xp.generate_2D_uniform_circular_sector(num_particles, r_range=(4, 6))
-y_norm, py_norm, _, _ = xp.generate_2D_uniform_circular_sector(num_particles, r_range=(4, 6))
-zeta, delta = xp.generate_longitudinal_coordinates(
-                    num_particles=num_particles, distribution='gaussian', sigma_z=7.55e-2, line=line
-            )
-theta = np.random.uniform(0, np.pi/2, num_particles)
-x_norm  *= np.cos(theta)
-px_norm *= np.cos(theta)
-y_norm  *= np.sin(theta)
-py_norm *= np.sin(theta)
-part = xp.build_particles(
-        x_norm=x_norm, px_norm=px_norm, y_norm=y_norm, py_norm=py_norm, zeta=zeta, delta=delta,
-        nemitt_x=3.5e-6, nemitt_y=3.5e-6, line=line, at_element=at_element
-)
-
-
 # Optimise the line
-line.optimize_for_tracking(keep_markers=[at_element])
-idx = line.element_names.index(at_element)
-part.at_element = idx
-part.start_tracking_at_element = idx
+line.optimize_for_tracking()
 
 
-# Do a pre-tracking to empty the halo
-coll_manager.enable_scattering()
-line.track(part, num_turns=pretrack_turns, time=True)
-coll_manager.disable_scattering()
-print(f"Done pre-tracking to empty halo in {line.time_last_track:.1f}s.")
+# Generate initial matched bunch
+part = xp.generate_matched_gaussian_bunch(nemitt_x=coll_manager.colldb.emittance[0],
+                                          nemitt_y=coll_manager.colldb.emittance[1],
+                                          sigma_z=7.55e-2, num_particles=num_particles, line=line)
 
-# We remove the lost particles, as we don't want them in the loss map
-part = part.filter(part.state == 1)
-print(f"{len(part.x)} particles left after initial tracking.")
 
 # Track during RF sweep:
 coll_manager.enable_scattering()
@@ -101,10 +73,9 @@ coll_manager.rf_sweep(sweep=sweep, num_turns=num_turns, particles=part, time=Tru
 coll_manager.disable_scattering()
 print(f"Done sweeping RF in {line.time_last_track:.1f}s.")
 
-
 # Save lossmap to json, which can be loaded, combined (for more statistics),
 # and plotted with the 'lossmaps' package
-_ = coll_manager.lossmap(part, file=Path(path_out,f'lossmap_B{beam}{plane}.json'))
+coll_manager.lossmap(part, file=Path(path_out,f'lossmap_B{beam}{plane}.json'))
 
 
 # Save a summary of the collimator losses to a text file
@@ -112,4 +83,51 @@ summary = coll_manager.summary(part, file=Path(path_out,f'coll_summary_B{beam}{p
 print(summary)
 
 
-exit()
+# Let's visualise how the losses move from IR7 to IR3 during the sweep
+tcp3 = line.element_names.index('tcp.6l3.b1')
+turns_ip3 = part.at_turn[part.at_element == tcp3]
+tcp7v = line.element_names.index('tcp.d6l7.b1')
+turns_ip7v = part.at_turn[part.at_element == tcp7v]
+tcp7h = line.element_names.index('tcp.c6l7.b1')
+turns_ip7h = part.at_turn[part.at_element == tcp7h]
+tcp7s = line.element_names.index('tcp.b6l7.b1')
+turns_ip7s = part.at_turn[part.at_element == tcp7s]
+
+plt.figure(figsize=(8,5))
+plt.hist([turns_ip7v, turns_ip7h, turns_ip7s, turns_ip3], 100, stacked=True,
+         label=['Lost on TCP.D7', 'Lost on TCP.C7', 'Lost on TCP.B7', 'Lost on TCP.3'])
+plt.xlabel('turn')
+plt.ylabel('number of lost particles (stacked)')
+plt.legend()
+plt.tight_layout()
+plt.savefig('OffMomentumPosition.png', dpi=300)
+plt.show()
+
+
+# In this example, the IR3 region gets populated very fast. When this is not the case, e.g. for injection,
+# one should keep only the last part of the sweep (as this is what happens in reality), as follows:
+
+
+# In reality the loss map only shows the last second (1.3s BLM integration time)
+# A typical RF sweep in reality shifts around 25Hz per second, i.e. around 2.2mHz/turn.
+# We sweep 50mHz/turn, so we are ~22.5 times faster than in reality.
+# We keep the equivalent of the last 3 seconds (slightly higher integration time, in order not to lose too much statistics).
+# We want to keep the last 3/22.5 seconds = 1500 turns.
+# This is equal to:
+seconds_to_keep = 3
+turns_to_keep = seconds_to_keep*25*num_turns/abs(sweep)
+last_turn = part.at_turn.max()
+part2 = part.filter(part.at_turn > last_turn - turns_to_keep)
+print(f"Keeping last {int(turns_to_keep)} turns (equivalent integration time of {seconds_to_keep} seconds).")
+print(f"This means we use {len(part2.x)} particles (of which {len(part2.x[part2.state > 0])} survived).")
+
+
+# Save lossmap to json, which can be loaded, combined (for more statistics),
+# and plotted with the 'lossmaps' package
+coll_manager.lossmap(part2, file=Path(path_out,f'lossmap_B{beam}{plane}_end.json'))
+
+
+# Save a summary of the collimator losses to a text file
+summary = coll_manager.summary(part2, file=Path(path_out,f'coll_summary_B{beam}{plane}_end.out'))
+print(summary)
+
