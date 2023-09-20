966639b537425582da89d5987e587c288dd00e50
Frederik Van der Veken <frederik@cern.ch>
2023-09-20 13:54:24 +0200

    Moved pdg to xpart (see PR #90)

--- xcoll/impacts/__init__.py
+++ xcoll/impacts/__init__.py
@@ -3,4 +3,3 @@
 # Copyright (c) CERN, 2023.                 #
 # ######################################### #
 
-from .pdg import make_ion_from_properties, get_particle_info_from_pdgid, get_element_name_from_Z, get_element_full_name_from_Z
