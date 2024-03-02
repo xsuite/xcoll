6ddddecedf13758ef563064bfe03ce9d5cf16be3
Frederik Van der Veken <frederik@cern.ch>
2024-03-02 01:24:02 +0100

    Updated transfer line example

--- examples/transfer_line_with_air.py
+++ examples/transfer_line_with_air.py
@@ -1,11 +1,9 @@
-import json
 import numpy as np
-from pathlib import Path
 import time
 start_time = time.time()
-import sys, os, contextlib
+import matplotlib.pyplot as plt
 
-import xobjects as xo
+import xpart as xp
 import xtrack as xt
 import xcoll as xc
 
@@ -87,7 +85,6 @@ y_norm, py_norm = xp.generate_2D_gaussian(num_part)
 part = line.build_particles(x_norm=x_norm, px_norm=px_norm, y_norm=y_norm, py_norm=py_norm,
                             W_matrix=tw.W_matrix[0], particle_on_co=line.particle_ref,
                             nemitt_x=nemitt_x,nemitt_y=nemitt_y)
-part._init_random_number_generator()
 
 # re-enable scattering
 line["Air 1"]._tracking = True
@@ -146,3 +143,7 @@ ax.plot(s, 1.e6*np.array(nemitt_y), label='V')
 ax.set_ylabel(r"$\epsilon\; [\mu\mathrm{m}]$")
 ax.set_xlabel("s [m]")
 ax.legend()
+
+print(f"Total calculation time {time.time()-start_time}s")
+plt.show()
+
