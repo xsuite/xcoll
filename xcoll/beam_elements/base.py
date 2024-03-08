fc8ef57eaef365b11b496e8f4c65e282867a35cd
Frederik Van der Veken <frederik@cern.ch>
2024-03-08 21:11:05 +0100

    Enable and disable scattering at the element itself

--- xcoll/beam_elements/base.py
+++ xcoll/beam_elements/base.py
@@ -60,6 +60,14 @@ class BaseBlock(xt.BeamElement):
         instance = super().__new__(cls)
         return instance
 
+    def enable_scattering(self):
+        if hasattr(self, '_tracking'):
+            self._tracking = True
+
+    def disable_scattering(self):
+        if hasattr(self, '_tracking'):
+            self._tracking = False
+
     def get_backtrack_element(self, _context=None, _buffer=None, _offset=None):
         return InvalidXcoll(length=-self.length,
                               _context=_context, _buffer=_buffer, _offset=_offset)
@@ -195,6 +203,14 @@ class BaseCollimator(xt.BeamElement):
 
         super().__init__(**kwargs)
 
+
+    def enable_scattering(self):
+        if hasattr(self, '_tracking'):
+            self._tracking = True
+
+    def disable_scattering(self):
+        if hasattr(self, '_tracking'):
+            self._tracking = False
 
     @property
     def jaw(self):
