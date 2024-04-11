a310fd703341d53c1b7b85f6dac48e1a3cc8fc2e
Frederik Van der Veken <frederik@cern.ch>
2024-04-11 22:16:48 +0200

    Moved geometry information into a struct, as casting the BeamElement to a BaseCollimator did not work (as the data fields are not necessarily at the same memory addresses)

--- xcoll/beam_elements/collimators_src/absorber.h
+++ xcoll/beam_elements/collimators_src/absorber.h
@@ -6,28 +6,54 @@
 #ifndef XCOLL_ABSORBER_H
 #define XCOLL_ABSORBER_H
 
+/*gpufun*/
+CollimatorGeometry BlackAbsorber_init_geometry(BlackAbsorberData el, LocalParticle* part0, int8_t active){
+    CollimatorGeometry cg = (CollimatorGeometry) malloc(sizeof(CollimatorGeometry_));
+    if (active){ // This is needed in order to avoid that the initialisation is called during a twiss!
+        // Collimator jaws (with tilts)
+        cg->jaw_LU = BlackAbsorberData_get__jaw_LU(el);
+        cg->jaw_LD = BlackAbsorberData_get__jaw_LD(el);
+        cg->jaw_RU = BlackAbsorberData_get__jaw_RU(el);
+        cg->jaw_RD = BlackAbsorberData_get__jaw_RD(el);
+        // TODO: need shortening of active length!
+        cg->length = BlackAbsorberData_get_length(el);
+        cg->side   = BlackAbsorberData_get__side(el);
+        // Get angles of jaws
+        cg->sin_zL = BlackAbsorberData_get__sin_zL(el);
+        cg->cos_zL = BlackAbsorberData_get__cos_zL(el);
+        cg->sin_zR = BlackAbsorberData_get__sin_zR(el);
+        cg->cos_zR = BlackAbsorberData_get__cos_zR(el);
+        cg->sin_zDiff = BlackAbsorberData_get__sin_zDiff(el);
+        cg->cos_zDiff = BlackAbsorberData_get__cos_zDiff(el);
+        cg->jaws_parallel = BlackAbsorberData_get__jaws_parallel(el);
+        // Impact table
+        cg->record = BlackAbsorberData_getp_internal_record(el, part0);
+        cg->record_index = NULL;
+        cg->record_touches = 0;
+        if (cg->record){
+            cg->record_index = CollimatorImpactsData_getp__index(cg->record);
+            cg->record_touches = BlackAbsorberData_get_record_touches(el);
+        }
+    }
+
+    return cg;
+}
+
 
 /*gpufun*/
 void BlackAbsorber_track_local_particle(BlackAbsorberData el, LocalParticle* part0){
 
     // Collimator active and length
-    int8_t is_active = BlackAbsorberData_get_active(el);
-    is_active       *= BlackAbsorberData_get__tracking(el);
+    int8_t active = BlackAbsorberData_get_active(el);
+    active       *= BlackAbsorberData_get__tracking(el);
     double const length = BlackAbsorberData_get_length(el);
 
-    // Impact table
-    CollimatorImpactsData record = BlackAbsorberData_getp_internal_record(el, part0);
-    RecordIndex record_index = NULL;
-    int8_t record_touches = 0;
-    int8_t record_interactions = 0;
-    if (record){
-        record_index = CollimatorImpactsData_getp__index(record);
-        record_touches = BlackAbsorberData_get_record_touches(el);
-        record_interactions = BlackAbsorberData_get_record_interactions(el);
-    }
+    // Get geometry
+    CollimatorGeometry cg      = BlackAbsorber_init_geometry(el, part0, active);
+    int8_t record_interactions = BlackAbsorberData_get_record_interactions(el);
 
     //start_per_particle_block (part0->part)
-        if (!is_active){
+        if (!active){
             // Drift full length
             Drift_single_particle(part, length);
 
@@ -38,17 +64,17 @@ void BlackAbsorber_track_local_particle(BlackAbsorberData el, LocalParticle* par
             if (is_tracking) {
 
                 // Check if hit on jaws
-                int8_t is_hit = hit_jaws_check_and_transform(part, (BaseCollimatorData) el, record, record_index, record_touches);
+                int8_t is_hit = hit_jaws_check_and_transform(part, cg);
 
                 if (is_hit != 0){
                     LocalParticle_set_state(part, XC_LOST_ON_ABSORBER);
                     if (record_interactions) {
-                        CollimatorImpactsData_log(record, record_index, part, XC_ABSORBED);  // In coll jaw reference frame
+                        CollimatorImpactsData_log(cg->record, cg->record_index, part, XC_ABSORBED);  // In coll jaw reference frame
                     }
                 }
 
                 // Transform back to the lab frame
-                hit_jaws_transform_back(is_hit, part, (BaseCollimatorData) el);
+                hit_jaws_transform_back(is_hit, part, cg);
             }
         }
     //end_per_particle_block
--- xcoll/beam_elements/collimators_src/collimator_geometry.h
+++ xcoll/beam_elements/collimators_src/collimator_geometry.h
@@ -8,35 +8,56 @@
 #include <math.h>
 #include <stdio.h>
 
+typedef struct CollimatorGeometry_ {
+    // Collimator jaws (with tilts)
+    double jaw_LU;
+    double jaw_LD;
+    double jaw_RU;
+    double jaw_RD;
+    // TODO: need shortening of active length!
+    double length;
+    int8_t side  ;
+    // Get angles of jaws
+    double sin_zL;
+    double cos_zL;
+    double sin_zR;
+    double cos_zR;
+    double sin_zDiff;
+    double cos_zDiff;
+    int8_t jaws_parallel;
+    // Impact table
+    CollimatorImpactsData record;
+    RecordIndex record_index;
+    int8_t record_touches;
+} CollimatorGeometry_;
+typedef CollimatorGeometry_ *CollimatorGeometry;
+
 
 // This function expects the particles to be rotated (in the XY plane) to the reference frame of the left jaw
 /*gpufun*/
-int8_t _hit_on_one_jaw(double x, int8_t side, double jaw_L, double jaw_R, int8_t jaws_parallel,
-                      LocalParticle* part, BaseCollimatorData el){
-    if (jaws_parallel){
-        if (x >= jaw_L && side != -1){
+int8_t _hit_on_one_jaw(double x, double jaw_L, double jaw_R, LocalParticle* part, CollimatorGeometry cg){
+    if (cg->jaws_parallel){
+        if (x >= jaw_L && cg->side != -1){
             return (int8_t) 1;   // Hit on left jaw
-        } else if (x <= jaw_R && side != 1){
+        } else if (x <= jaw_R && cg->side != 1){
             return (int8_t) -1;  // Hit on right jaw
         } else {
             return (int8_t) 0;   // Survived
         }
 
     } else {
-        if (x >= jaw_L && side != -1){
+        if (x >= jaw_L && cg->side != -1){
             return (int8_t) 1;   // Hit on left jaw
-        } else if (side == 1){   // Left-sided collimator
+        } else if (cg->side == 1){   // Left-sided collimator
             return (int8_t) 0;   // Survived
         } else {
             // Right jaw is angled w.r.t. to left jaw. Hence, we move to the frame of the right jaw by rotating the difference.
-            double const sin_zDiff = BaseCollimatorData_get__sin_zDiff(el);
-            double const cos_zDiff = BaseCollimatorData_get__cos_zDiff(el);
-            SRotation_single_particle(part, sin_zDiff, cos_zDiff);
+            SRotation_single_particle(part, cg->sin_zDiff, cg->cos_zDiff);
             if (x <= jaw_R){
                 return (int8_t) -1;  // Hit on right jaw
             } else {
                 // Rotate back to left frame.
-                SRotation_single_particle(part, -sin_zDiff, cos_zDiff);
+                SRotation_single_particle(part, -cg->sin_zDiff, cg->cos_zDiff);
                 return (int8_t) 0;   // Survived
             }
         }
@@ -51,78 +72,58 @@ int8_t _hit_on_one_jaw(double x, int8_t side, double jaw_L, double jaw_R, int8_t
 //              and transformed to the reference frame of that jaw.
 
 /*gpufun*/
-int8_t hit_jaws_check_and_transform(LocalParticle* part, BaseCollimatorData el, CollimatorImpactsData record,
-                                    RecordIndex record_index, int8_t record_touches){
-    // Get data
-    // ========
-
-    // Collimator jaws (with tilts)
-    double const jaw_LU = BaseCollimatorData_get__jaw_LU(el);
-    double const jaw_LD = BaseCollimatorData_get__jaw_LD(el);
-    double const jaw_RU = BaseCollimatorData_get__jaw_RU(el);
-    double const jaw_RD = BaseCollimatorData_get__jaw_RD(el);
-    // TODO: need shortening of active length!
-    double const length = BaseCollimatorData_get_length(el);
-    int8_t const side   = BaseCollimatorData_get__side(el);
-
-    // Get angles of jaws
-    double const sin_zL = BaseCollimatorData_get__sin_zL(el);
-    double const cos_zL = BaseCollimatorData_get__cos_zL(el);
-    int8_t const jaws_parallel = BaseCollimatorData_get__jaws_parallel(el);
-
-    // Do the tracking
-    // ==============
-
+int8_t hit_jaws_check_and_transform(LocalParticle* part, CollimatorGeometry cg){
     double x_U, x_D, backtrack_length;
 
     // Rotate to the frame of the left jaw
-    SRotation_single_particle(part, sin_zL, cos_zL);
+    SRotation_single_particle(part, cg->sin_zL, cg->cos_zL);
 
     // Check if hit on the collimator jaws at the front
     x_U = LocalParticle_get_x(part);
-    int8_t is_hit = _hit_on_one_jaw(x_U, side, jaw_LU, jaw_RU, jaws_parallel, part, el);
+    int8_t is_hit = _hit_on_one_jaw(x_U, cg->jaw_LU, cg->jaw_RU, part, cg);
 
-    // Log the hit
-    if (record_touches){
-        if (is_hit == 1){
-            // Hit on left upstream jaw
-            CollimatorImpactsData_log(record, record_index, part, XC_ENTER_JAW_L);
-    
-        } else if (is_hit == -1){
-            // Hit on right upstream jaw
-            CollimatorImpactsData_log(record, record_index, part, XC_ENTER_JAW_R);
-    
+    if (is_hit == 1){
+        // Hit on left upstream jaw
+        if (cg->record_touches){
+            CollimatorImpactsData_log(cg->record, cg->record_index, part, XC_ENTER_JAW_L);
         }
+
+    } else if (is_hit == -1){
+        // Hit on right upstream jaw
+        if (cg->record_touches){
+            CollimatorImpactsData_log(cg->record, cg->record_index, part, XC_ENTER_JAW_R);
+        }
+
     }
 
     // Continue if the particle didn't hit the collimator front
     if (is_hit == 0){
-        Drift_single_particle(part, length);
+        Drift_single_particle(part, cg->length);
 
         // Check if hit on the collimator jaw at the back
         x_D = LocalParticle_get_x(part);
-        is_hit = _hit_on_one_jaw(x_D, side, jaw_LD, jaw_RD, jaws_parallel, part, el);
+        is_hit = _hit_on_one_jaw(x_D, cg->jaw_LD, cg->jaw_RD, part, cg);
 
         // If hit, backtrack to the particle position of impact
         if (is_hit == 1){
             // Hit on left downstream jaw
-            backtrack_length = (jaw_LD - x_D) / (jaw_LD - jaw_LU - x_D + x_U) * length;
+            backtrack_length = (cg->jaw_LD - x_D) / (cg->jaw_LD - cg->jaw_LU - x_D + x_U) * cg->length;
             Drift_single_particle(part, -backtrack_length);
-            if (record_touches){
-                CollimatorImpactsData_log(record, record_index, part, XC_ENTER_JAW_L);
+            if (cg->record_touches){
+                CollimatorImpactsData_log(cg->record, cg->record_index, part, XC_ENTER_JAW_L);
             }
 
         } else if (is_hit == -1){
             // Hit on right downstream jaw
-            backtrack_length = (jaw_RD - x_D) / (jaw_RD - jaw_RU - x_D + x_U) * length;
+            backtrack_length = (cg->jaw_RD - x_D) / (cg->jaw_RD - cg->jaw_RU - x_D + x_U) * cg->length;
             Drift_single_particle(part, -backtrack_length);
-            if (record_touches){
-                CollimatorImpactsData_log(record, record_index, part, XC_ENTER_JAW_R);
+            if (cg->record_touches){
+                CollimatorImpactsData_log(cg->record, cg->record_index, part, XC_ENTER_JAW_R);
             }
 
         } else {
             // If still not hit, return to lab frame
-            SRotation_single_particle(part, -sin_zL, cos_zL);
+            SRotation_single_particle(part, -cg->sin_zL, cg->cos_zL);
 
         }
     }
@@ -132,17 +133,13 @@ int8_t hit_jaws_check_and_transform(LocalParticle* part, BaseCollimatorData el, 
 
 
 /*gpufun*/
-void hit_jaws_transform_back(int8_t is_hit, LocalParticle* part, BaseCollimatorData el){
+void hit_jaws_transform_back(int8_t is_hit, LocalParticle* part, CollimatorGeometry cg){
     if (is_hit == 1){
         // Hit on left jaw
-        double const sin_zL = BaseCollimatorData_get__sin_zL(el);
-        double const cos_zL = BaseCollimatorData_get__cos_zL(el);
-        SRotation_single_particle(part, -sin_zL, cos_zL);
+        SRotation_single_particle(part, -cg->sin_zL, cg->cos_zL);
     } else if (is_hit == -1){
         // Hit on right jaw
-        double const sin_zR = BaseCollimatorData_get__sin_zR(el);
-        double const cos_zR = BaseCollimatorData_get__cos_zR(el);
-        SRotation_single_particle(part, -sin_zR, cos_zR);
+        SRotation_single_particle(part, -cg->sin_zR, cg->cos_zR);
     }
 }
 
