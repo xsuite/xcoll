29f2fc044a74ab3939e0e3a3af981b61089068db
Frederik F. Van der Veken <frederik@cern.ch>
2024-05-14 03:02:40 +0200

    Updated test for new geometry (still need tests with vlimit)

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -3,6 +3,7 @@
 # Copyright (c) CERN, 2024.                 #
 # ######################################### #
 
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,81 +12,199 @@ import json
 import xobjects as xo
 import xcoll as xc
 
+sys.path.insert(1, (xc._pkg_root.parent / 'tests' / 'data').as_posix())
+from xcoll_geometry_test import XcollGeometryTest
 
-def test_closed_polygon():
-    kernels = _create_geometry_kernel()
+
+def _init_kernels():
+    geom = XcollGeometryTest()
+    geom.compile_kernels(only_if_needed=True)#, save_source_as='geometry.c')
+    return geom._context.kernels
+
+kernels = _init_kernels()
+
+
+def test_jaw():
     with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
-        expected_s = json.load(fp)['expected_s']
-    for tilt in expected_s.keys():
-        poly_s, poly_x, _, _ = _generate_polygon_points(8, tilt_L=int(tilt))
-        for part_ang in expected_s[tilt].keys():
-            part_tan = np.tan(np.deg2rad(int(part_ang)))
-            for part_x_cm in range(-60, 80):
-                part_x = part_x_cm/100
-                s = kernels.check_poly(part_x=part_x, part_tan=part_tan, poly_s=poly_s,
-                                       poly_x=poly_x, num_polys=len(poly_s), is_closed=True)
-                if s > 1.e10:
-                    s = None
-                    assert part_x_cm not in expected_s[tilt][part_ang]
-                else:
-                    assert np.isclose(s, expected_s[tilt][part_ang][str(part_x_cm)])
-                # _plot_poly(part_x, part_tan, poly_s, poly_x, s=s)   # Visual inspection of test validity
-
-def test_open_polygon():
-    kernels = _create_geometry_kernel()
-    # with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
-    #     expected_s = json.load(fp)['expected_s_open']
-    expected_s = {}
+        expected_s = json.load(fp)['expected_s_jaw']
     for tilt_L in expected_s.keys():
+        tilt_tan_L = np.tan(np.deg2rad(int(tilt_L)))
         for tilt_R in expected_s[tilt_L].keys():
-            poly_s_L, poly_x_L, poly_s_R, poly_x_R = _generate_polygon_points(8, tilt_L=int(tilt_L), tilt_R=int(tilt_R))
+            tilt_tan_R = np.tan(np.deg2rad(int(tilt_R)))
+            s_poly_L, x_poly_L, s_poly_R, x_poly_R = _generate_polygon_points(8, tilt_L=int(tilt_L), tilt_R=int(tilt_R))
             for part_ang in expected_s[tilt_L][tilt_R].keys():
-                part_tan = np.tan(np.deg2rad(int(part_ang)))
+                part_tan_x = np.tan(np.deg2rad(int(part_ang)))
                 for part_x_cm in range(-90, 90):
                     part_x = part_x_cm/100
-                    s_L = kernels.check_open_poly(part_x=part_x, part_tan=part_tan, poly_s=poly_s_L, poly_x=poly_x_L,
-                                                  num_polys=len(poly_s_L), tan_tilt=np.tan(np.deg2rad(tilt_L)), side=1)
-                    s_R = kernels.check_open_poly(part_x=part_x, part_tan=part_tan, poly_s=poly_s_R, poly_x=poly_x_R,
-                                                  num_polys=len(poly_s_R), tan_tilt=np.tan(np.deg2rad(tilt_R)), side=-1)
+                    s_L = kernels.test_jaw(part_x=part_x, part_tan_x=part_tan_x, s_U=s_poly_L[1], x_U=x_poly_L[1],
+                                           s_D=s_poly_L[2], x_D=x_poly_L[2], tilt_tan=tilt_tan_L, side=1)
+                    s_R = kernels.test_jaw(part_x=part_x, part_tan_x=part_tan_x, s_U=s_poly_R[1], x_U=x_poly_R[1],
+                                           s_D=s_poly_R[2], x_D=x_poly_R[2], tilt_tan=tilt_tan_R, side=-1)
                     s = min(s_L, s_R)
                     if s > 1.e10:
                         s = None
                         assert part_x_cm not in expected_s[tilt_L][tilt_R][part_ang]
                     else:
                         assert np.isclose(s, expected_s[tilt_L][tilt_R][part_ang][str(part_x_cm)])
-                    # _plot_poly(part_x, part_tan, poly_s, poly_x, s=s)   # Visual inspection of test validity
 
 
-def _create_geometry_kernel():
-    src_poly = xc._pkg_root / 'scattering_routines' / 'geometry' / 'polygon.h'
-    kernels_poly = {
-        'check_poly': xo.Kernel(
-                c_name='get_s_of_first_crossing_with_polygon',
-                args=[
-                    xo.Arg(xo.Float64, pointer=False, name='part_x'),
-                    xo.Arg(xo.Float64, pointer=False, name='part_tan'),
-                    xo.Arg(xo.Float64, pointer=True, name='poly_s'),
-                    xo.Arg(xo.Float64, pointer=True, name='poly_x'),
-                    xo.Arg(xo.Int8, name='num_polys'),
-                    xo.Arg(xo.Int8, name='is_closed')
-                ],
-                ret=xo.Arg(xo.Float64, pointer=False, name='s')),
-        'check_open_poly': xo.Kernel(
-                c_name='get_s_of_first_crossing_with_open_polygon',
-                args=[
-                    xo.Arg(xo.Float64, pointer=False, name='part_x'),
-                    xo.Arg(xo.Float64, pointer=False, name='part_tan'),
-                    xo.Arg(xo.Float64, pointer=True, name='poly_s'),
-                    xo.Arg(xo.Float64, pointer=True, name='poly_x'),
-                    xo.Arg(xo.Int8, name='num_polys'),
-                    xo.Arg(xo.Float64, name='tan_tilt'),
-                    xo.Arg(xo.Int8, name='side')
-                ],
-                ret=xo.Arg(xo.Float64, pointer=False, name='s'))
-    }
-    context = xo.ContextCpu()
-    context.add_kernels(sources=[src_poly], kernels=kernels_poly)
-    return context.kernels
+def test_jaw_after_s():
+    with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
+        expected_s = json.load(fp)['expected_s_jaw_after_s']
+    for tilt_L in expected_s.keys():
+        tilt_tan_L = np.tan(np.deg2rad(int(tilt_L)))
+        for tilt_R in expected_s[tilt_L].keys():
+            tilt_tan_R = np.tan(np.deg2rad(int(tilt_R)))
+            s_poly_L, x_poly_L, s_poly_R, x_poly_R = _generate_polygon_points(8, tilt_L=int(tilt_L), tilt_R=int(tilt_R))
+            for part_ang in expected_s[tilt_L][tilt_R].keys():
+                part_tan_x = np.tan(np.deg2rad(int(part_ang)))
+                for part_x_cm in range(-90, 90):
+                    part_x = part_x_cm/100
+                    s_L = kernels.test_jaw_after_s(part_x=part_x, part_tan_x=part_tan_x, s_U=s_poly_L[1], x_U=x_poly_L[1],
+                                           s_D=s_poly_L[2], x_D=x_poly_L[2], tilt_tan=tilt_tan_L, side=1, current_s=0.7)
+                    s_R = kernels.test_jaw_after_s(part_x=part_x, part_tan_x=part_tan_x, s_U=s_poly_R[1], x_U=x_poly_R[1],
+                                           s_D=s_poly_R[2], x_D=x_poly_R[2], tilt_tan=tilt_tan_R, side=-1, current_s=0.7)
+                    s = min(s_L, s_R)
+                    if s > 1.e10:
+                        s = None
+                        assert part_x_cm not in expected_s[tilt_L][tilt_R][part_ang]
+                    else:
+                        assert np.isclose(s, expected_s[tilt_L][tilt_R][part_ang][str(part_x_cm)])
+
+
+def test_polygon():
+    kernels = _init_kernels()
+    with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
+        expected_s = json.load(fp)['expected_s_polygon']
+    for tilt in expected_s.keys():
+        s_poly, x_poly, _, _ = _generate_polygon_points(8, tilt_L=int(tilt))
+        for part_ang in expected_s[tilt].keys():
+            part_tan_x = np.tan(np.deg2rad(int(part_ang)))
+            for part_x_cm in range(-60, 80):
+                part_x = part_x_cm/100
+                s = kernels.test_polygon(part_x=part_x, part_tan_x=part_tan_x, s_poly=s_poly,
+                                         x_poly=x_poly, num_polys=len(s_poly))
+                if s > 1.e10:
+                    s = None
+                    assert part_x_cm not in expected_s[tilt][part_ang]
+                else:
+                    assert np.isclose(s, expected_s[tilt][part_ang][str(part_x_cm)])
+                # _plot_poly(part_x, part_tan_x, s_poly, x_poly, s=s)   # Visual inspection of test validity
+
+
+def test_polygon_after_s():
+    kernels = _init_kernels()
+    with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
+        expected_s = json.load(fp)['expected_s_polygon_after_s']
+    for tilt in expected_s.keys():
+        s_poly, x_poly, _, _ = _generate_polygon_points(8, tilt_L=int(tilt))
+        for part_ang in expected_s[tilt].keys():
+            part_tan_x = np.tan(np.deg2rad(int(part_ang)))
+            for part_x_cm in range(-60, 80):
+                part_x = part_x_cm/100
+                s = kernels.test_polygon_after_s(part_x=part_x, part_tan_x=part_tan_x, s_poly=s_poly,
+                                                 x_poly=x_poly, num_polys=len(s_poly), current_s=0.7)
+                if s > 1.e10:
+                    s = None
+                    assert part_x_cm not in expected_s[tilt][part_ang]
+                else:
+                    assert np.isclose(s, expected_s[tilt][part_ang][str(part_x_cm)])
+                # _plot_poly(part_x, part_tan_x, s_poly, x_poly, s=s)   # Visual inspection of test validity
+
+
+def test_open_polygon():
+    kernels = _init_kernels()
+    with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
+        expected_s = json.load(fp)['expected_s_open_polygon']
+    for tilt_L in expected_s.keys():
+        tilt_tan_L = np.tan(np.deg2rad(int(tilt_L)))
+        for tilt_R in expected_s[tilt_L].keys():
+            tilt_tan_R = np.tan(np.deg2rad(int(tilt_R)))
+            s_poly_L, x_poly_L, s_poly_R, x_poly_R = _generate_polygon_points(8, tilt_L=int(tilt_L), tilt_R=int(tilt_R))
+            for part_ang in expected_s[tilt_L][tilt_R].keys():
+                part_tan_x = np.tan(np.deg2rad(int(part_ang)))
+                for part_x_cm in range(-90, 90):
+                    part_x = part_x_cm/100
+                    s_L = kernels.test_open_polygon(part_x=part_x, part_tan_x=part_tan_x, s_poly=s_poly_L, x_poly=x_poly_L,
+                                                    num_polys=len(s_poly_L), tilt_tan=tilt_tan_L, side=1)
+                    s_R = kernels.test_open_polygon(part_x=part_x, part_tan_x=part_tan_x, s_poly=s_poly_R, x_poly=x_poly_R,
+                                                    num_polys=len(s_poly_R), tilt_tan=tilt_tan_R, side=-1)
+                    s = min(s_L, s_R)
+                    if s > 1.e10:
+                        s = None
+                        assert part_x_cm not in expected_s[tilt_L][tilt_R][part_ang]
+                    else:
+                        assert np.isclose(s, expected_s[tilt_L][tilt_R][part_ang][str(part_x_cm)])
+                    # _plot_poly(part_x, part_tan_x, s_poly, x_poly, s=s)   # Visual inspection of test validity
+
+
+def test_open_polygon_after_s():
+    kernels = _init_kernels()
+    with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
+        expected_s = json.load(fp)['expected_s_open_polygon_after_s']
+    for tilt_L in expected_s.keys():
+        tilt_tan_L = np.tan(np.deg2rad(int(tilt_L)))
+        for tilt_R in expected_s[tilt_L].keys():
+            tilt_tan_R = np.tan(np.deg2rad(int(tilt_R)))
+            s_poly_L, x_poly_L, s_poly_R, x_poly_R = _generate_polygon_points(8, tilt_L=int(tilt_L), tilt_R=int(tilt_R))
+            for part_ang in expected_s[tilt_L][tilt_R].keys():
+                part_tan_x = np.tan(np.deg2rad(int(part_ang)))
+                for part_x_cm in range(-90, 90):
+                    part_x = part_x_cm/100
+                    s_L = kernels.test_open_polygon_after_s(part_x=part_x, part_tan_x=part_tan_x, s_poly=s_poly_L, x_poly=x_poly_L,
+                                                    num_polys=len(s_poly_L), tilt_tan=tilt_tan_L, side=1, current_s=0.7)
+                    s_R = kernels.test_open_polygon_after_s(part_x=part_x, part_tan_x=part_tan_x, s_poly=s_poly_R, x_poly=x_poly_R,
+                                                    num_polys=len(s_poly_R), tilt_tan=tilt_tan_R, side=-1, current_s=0.7)
+                    s = min(s_L, s_R)
+                    if s > 1.e10:
+                        s = None
+                        assert part_x_cm not in expected_s[tilt_L][tilt_R][part_ang]
+                    else:
+                        assert np.isclose(s, expected_s[tilt_L][tilt_R][part_ang][str(part_x_cm)])
+                    # _plot_poly(part_x, part_tan_x, s_poly, x_poly, s=s)   # Visual inspection of test validity
+
+
+def test_crystal():
+    kernels = _init_kernels()
+    with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
+        expected_s = json.load(fp)['expected_s_crystal']
+    for tilt in expected_s.keys():
+        tilt_sin = np.sin(np.deg2rad(int(tilt)))
+        tilt_cos = np.cos(np.deg2rad(int(tilt)))
+        for R in expected_s[tilt].keys():
+            for part_ang in expected_s[tilt][R].keys():
+                part_tan_x = np.tan(np.deg2rad(int(part_ang)))
+                for part_x_cm in range(-60, 80):
+                    part_x = part_x_cm/100
+                    s = kernels.test_crystal(part_x=part_x, part_tan_x=part_tan_x, R=float(R), width=0.1476,
+                                      length=1.6597, jaw_U=0.112, tilt_sin=tilt_sin, tilt_cos=tilt_cos)
+                    if s > 1.e10:
+                        s = None
+                        assert part_x_cm not in expected_s[tilt][R][part_ang]
+                    else:
+                        assert np.isclose(s, expected_s[tilt][R][part_ang][str(part_x_cm)])
+
+
+def test_crystal_after_s():
+    kernels = _init_kernels()
+    with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
+        expected_s = json.load(fp)['expected_s_crystal_after_s']
+    for tilt in expected_s.keys():
+        tilt_sin = np.sin(np.deg2rad(int(tilt)))
+        tilt_cos = np.cos(np.deg2rad(int(tilt)))
+        for R in expected_s[tilt].keys():
+            for part_ang in expected_s[tilt][R].keys():
+                part_tan_x = np.tan(np.deg2rad(int(part_ang)))
+                for part_x_cm in range(-60, 80):
+                    part_x = part_x_cm/100
+                    s = kernels.test_crystal_after_s(part_x=part_x, part_tan_x=part_tan_x, R=float(R), width=0.1476,
+                                      length=1.6597, jaw_U=0.112, tilt_sin=tilt_sin, tilt_cos=tilt_cos, current_s=0.7)
+                    if s > 1.e10:
+                        s = None
+                        assert part_x_cm not in expected_s[tilt][R][part_ang]
+                    else:
+                        assert np.isclose(s, expected_s[tilt][R][part_ang][str(part_x_cm)])
+
 
 def _generate_polygon_points(num_poly, tilt_L=0, tilt_R=0):
     with open(xc._pkg_root.parent / "tests" / "data" / "geometry.json", "r") as fp:
