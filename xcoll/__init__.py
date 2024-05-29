f8594ee7d0010887393588206aec89d9dc39e8cc
Frederik Van der Veken <frederik@cern.ch>
2024-05-29 02:06:21 +0200

    Small typo in package init

--- xcoll/__init__.py
+++ xcoll/__init__.py
@@ -9,12 +9,14 @@ from .beam_elements import BlackAbsorber, BlackCrystal, EverestBlock, EverestCol
 from .install import install_elements
 from .line_tools import assign_optics_to_collimators, open_collimators, send_to_parking, enable_scattering, disable_scattering
 from .scattering_routines.everest import materials, Material, CrystalMaterial
-from .manager import CollimatorManager
-from .colldb import CollimatorDatabase, load_SixTrack_colldb
+from .colldb import CollimatorDatabase
 from .interaction_record import InteractionRecord
 from .rf_sweep import RFSweep
 from .initial_distribution import *
 from .lossmap import LossMap
+
+# Deprecated
+from .manager import CollimatorManager
 
 # print("If you use Xcoll in your simulations, please cite us :-)")
 # print(citation)
