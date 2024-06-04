053714558be9143dfe4204bde11ae80876f155f1
Bjorn Lindstrom <bjorn.lindstrom@cern.ch>
2024-06-04 16:56:49 +0200

    Fixed various issues in install script

--- install_bdsim.sh
+++ install_bdsim.sh
@@ -1,64 +1,38 @@
-echo "This script will download, build and install geant4 and bdsim,"
-echo " which are necessary for using the geant4 scattering module."
-echo
-echo "The recommended way is to set up a new environment using mamba or conda."
-echo
+#!/bin/bash
 
-function setupEnvironment(){
-    read -p "Do you prefer mamba or conda? [mamba/conda]" envexe
-    if command -v $envexe &> /dev/null
-    then
-        echo $envexe" could not be found, checking if already installed..."
-        if [ -d "~/miniconda3" ]; then
-            echo "found it, initializing..."
-            eval "$(~/miniconda3/bin/"$envexe" shell.bash hook)"
-        else
-            mkdir miniconda
-            cd miniconda
-            wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-            bash Miniconda3-latest-Linux-x86_64.sh
-            eval "$(~/miniconda3/bin/"$envexe" shell.bash hook)"
-        fi
-    fi
-    read -p "Input name of your new environment: " envname
-    $envexe create --name $envname python=3.9
-    $envexe activate $envname
-    pip install numpy scipy matplotlib pandas ipython pytest pyyaml numexpr schema tables
+function GOTO() {
+    cmd=$(sed -n "/:$1:/{:a;n;p;ba};" $0 | grep -v ':$')
+    eval "$cmd"
+}     
 
-    read -p "Do you want to download and install the other xsuite packages?" yn
-    case $yn in
-        [Yy]* ) echo "downloading and installing in the parent directory..."; cd ../ ; git clone https://github.com/xsuite/xobjects ; git clone https://github.com/xsuite/xdeps ; git clone https://github.com/xsuite/xpart ; git clone https://github.com/xsuite/xtrack ; git clone https://github.com/xsuite/xfields ; pip install -e xobjects xdeps xpart xtrack xfields ; cd xcoll ;;
-        [Nn]* ) echo "please pip install them manually in this environment."; ;;
-        * ) echo "Please answer y or n";;
-    esac
+GOTO 'colors'
 
-    $envexe config --add channels conda-forge
-    $envexe config --set channel_priority strict
-    $envexe install compilers cmake make
-    $envexe install root=6.28.12  # to get root version compiled with C++17
-    $envexe install clhep=2.4.5.3
-    $envexe install xerces-c
-    $envexe install boost
-    $envexe install qt-main
-    $envexe install mesa-libgl-devel-cos7-x86_64 mesa-dri-drivers-cos7-x86_64 libselinux-cos7-x86_64 libxdamage-cos7-x86_64 libxxf86vm-cos7-x86_64 libxext-cos7-x86_64
-    $envexe install xorg-libxmu
-    $envexe install flex bison   
-}
+:start:
+echo -e "${LIMEGREEN}This script will download, build and install geant4 and bdsim,"
+echo -e " which are necessary for using the geant4 scattering module.\n"
+echo -e "The recommended way is to set up a new environment using mamba or conda.\n${RESET}"
 
+GOTO 'fundefs'
+
+:main:
 xcollpath=$(pwd)
 cd ../
 rootpath=$(pwd)
 cd $xcollpath
 
-read -p "Set up conda or mamba environment? [y/n]" yn
-case $yn in
-    [Yy]* ) echo "setting up environment..."; setupEnvironment ;;
-    [Nn]* ) echo "will not set up environment or install any packages, if geant4 or bdsim cannot be built, please do it manually"; ;;
-    * ) echo "Please answer y or n";;
-esac
+while true; do
+    read -p "$(echo -e ${CYAN}Set up conda or mamba environment? [y/n] ${RESET})" yn
+    case $yn in
+        [Yy]* ) echo -e "${YELLOW}setting up environment...${RESET}"; setupEnvironment ;
+                break ;;
+        [Nn]* ) echo -e "${YELLOW}will not set up environment or install any packages, if geant4 or bdsim cannot be built, please do it manually${RESET}";
+                break ;;
+        * ) echo -e "${YELLOW}Please answer y or n ${RESET}";;
+    esac
+done
 
 cd $rootpath
-echo "downloading geant4..."
+echo -e "${YELLOW}downloading geant4...${RESET}"
 wget https://gitlab.cern.ch/geant4/geant4/-/archive/v10.4.3/geant4-v10.4.3.tar.gz
 tar -xvf geant4-v10.4.3.tar.gz
 mkdir -p geant4-v10.4.3/build
@@ -78,7 +52,7 @@ make install
 cd $rootpath
 
 echo
-echo "downloading bdsim..."
+echo -e "${YELLOW}downloading bdsim...${RESET}"
 git clone --recursive https://bitbucket.org/jairhul/bdsim
 
 # edit bdsim/CMakeLists.txt to change CMAKE_CXX_STANDARD to 17
@@ -86,29 +60,122 @@ sed -i 's/set(CMAKE_CXX_STANDARD [0-9]\+)/set(CMAKE_CXX_STANDARD 17)/' bdsim/CMa
 mkdir -p bdsim/build
 source $rootpath/geant4-v10.4.3/bin/geant4.sh
 cd bdsim/build
-cmake $rootpath/bdsim \
+cmake ../../bdsim \
     -DCMAKE_INSTALL_PREFIX=$rootpath/bdsim \
     -DUSE_SIXTRACKLINK=ON \
     -DBDSIM_BUILD_STATIC_LIBS=ON \
-    -DGEANT4_CONFIG=$rootpath/geant4-v10.4.3/bin/geant4-config \
+    -DGEANT4_CONFIG=$rootpath/geant4-v10.4.3/bin/geant4-config
 make -j $(nproc) 
 make install
 unset LD_LIBRARY_PATH
 
 echo
-echo "compiling collimasim..."
+echo -e "${YELLOW}compiling collimasim...${RESET}"
 cd $xcollpath
 # check if collimasim is already downloaded
 if [ ! -d "xcoll/scattering_routines/geant4/collimasim" ];
 then
-    echo "collimasim not found, downloading..."
+    echo -e "${YELLOW}collimasim not found, downloading...${RESET}"
     pushd xcoll/scattering_routines/geant4
     git clone --recurse-submodules https://gitlab.cern.ch/anabramo/collimasim.git
     if [ $? -ne 0 ]; then
-        echo "ERROR: failued to clone collimasim repository"
+        echo -e "${RED}ERROR: failed to clone collimasim repository${RESET}"
         popd
         exit 1
     fi
     popd
 fi     
 ./compile_collimasim.sh
+
+exit 0
+
+:colors:
+# Color codes
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+MAGENTA='\033[35m'
+CYAN='\033[36m'
+WHITE='\033[37m'
+BOLD='\033[1m'
+RESET='\033[0m'
+LIMEGREEN='\033[38;5;10m'  
+
+GOTO 'start'      
+
+:fundefs:
+# Global error handler
+handle_error() {
+    echo "${RED}ERROR on line $1"
+    echo "exiting...${RESET}"
+    exit 1
+}
+trap 'handle_error $LINENO' ERR    
+
+function setupEnvironment(){
+    while true; do
+        read -p "$(echo -e ${CYAN}Do you prefer mamba or conda? [mamba/conda] ${RESET})" envexe
+        if [[ "$envexe" == "mamba" || "$envexe" == "conda" ]]; then
+            break
+        else
+            echo -e "${YELLOW}Please enter 'mamba' or 'conda'${RESET}"
+        fi
+    done    
+    ### checking if the executable is accessible
+    if ! command -v $envexe &> /dev/null; then
+        echo -e "${YELLOW}$envexe could not be found, checking if already installed...${RESET}"
+        if [ -d "$HOME/miniconda3" ]; then
+            echo -e "${LIMEGREEN}found it, initializing...${RESET}"
+            eval "$($HOME/miniconda3/bin/$envexe shell.bash hook)"
+        else
+            mkdir -p miniconda
+            cd miniconda
+            wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+            bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+            eval "$($HOME/miniconda3/bin/$envexe shell.bash hook)"
+            cd ..
+        fi
+    fi
+
+    read -p "$(echo -e ${CYAN}Input name of your new environment: ${RESET})" envname
+    $envexe create --name $envname python=3.9
+    source $($envexe info --base)/bin/activate $envname
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Error: Failed to activate environment $envname.${RESET}"
+        exit 1
+    fi
+    pip install numpy scipy matplotlib pandas ipython pytest pyyaml numexpr schema tables
+
+    while true; do
+        read -p "$(echo -e ${CYAN}Do you want to download and install the other xsuite packages? ${RESET})" yn
+        case $yn in
+            [Yy]* ) echo -e "${YELLOW}downloading and installing in the parent directory...${RESET}"; cd ../ ; 
+                    git clone https://github.com/xsuite/xobjects ; 
+                    git clone https://github.com/xsuite/xdeps ; 
+                    git clone https://github.com/xsuite/xpart ; 
+                    git clone https://github.com/xsuite/xtrack ; 
+                    git clone https://github.com/xsuite/xfields ; 
+                    pip install -e xobjects xdeps xpart xtrack xfields ; 
+                    cd xcoll ;
+                    break ;;
+            [Nn]* ) echo -e "${YELLOW}please pip install them manually in this environment.${RESET}";
+                    break ;;
+            * ) echo -e "${YELLOW}Please answer y or n${RESET}";;
+        esac
+    done
+
+    $envexe config --add channels conda-forge
+    $envexe config --set channel_priority strict
+    $envexe install compilers cmake make -y
+    $envexe install root=6.28.12 -y # to get root version compiled with C++17
+    $envexe install clhep=2.4.5.3 -y
+    $envexe install xerces-c -y
+    $envexe install boost -y
+    $envexe install qt-main -y
+    $envexe install mesa-libgl-devel-cos7-x86_64 mesa-dri-drivers-cos7-x86_64 libselinux-cos7-x86_64 libxdamage-cos7-x86_64 libxxf86vm-cos7-x86_64 libxext-cos7-x86_64 -y
+    $envexe install xorg-libxmu -y
+    $envexe install flex bison -y
+}
+
+GOTO 'main'        
