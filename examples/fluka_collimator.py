affe786adfd85e517a453819f44cc809c410a5c9
Frederik Van der Veken <frederik@cern.ch>
2024-07-03 15:51:04 +0200

    Expanded example with single FLUKA collimator a bit

--- examples/fluka_collimator.py
+++ examples/fluka_collimator.py
@@ -11,44 +11,86 @@ import xpart as xp
 import xcoll as xc
 import time
 
+import matplotlib.pyplot as plt
 
-_capacity = 20000
 
-coll = xc.FlukaCollimator(length=0.753)
+num_part = int(10000)
+_capacity = num_part*2
+
+# Create a FLUKA collimator
+coll = xc.FlukaCollimator(length=0.6)
+coll_name = 'tcp.c6l7.b1'
 coll.jaw = 0.001
 coll.gap = 1  # STUB, needed to make FLUKAbuilder succeed but so far no clue why
 
-print(coll.to_dict())
+# The same collimator in Everest
+coll2 = xc.EverestCollimator(length=0.6, material=xc.materials.MolybdenumGraphite)
+coll2.jaw = 0.001
 
 
-# Connect to FLUKA
-xc.FlukaEngine.start(elements=coll, names='tcp.c6l7.b1', debug_level=1, _capacity=_capacity)
-particle_ref = xp.Particles.reference_from_pdg_id(pdg_id='proton', p0c=6.8e12)
-xc.FlukaEngine.set_particle_ref(particle_ref=particle_ref)
-
-# Create an initial distribution of particles, random in 4D (with the
+# Create an initial distribution of particles, random in 4D, on the left jaw (with the
 # longitudinal coordinates set to zero)
-num_part = int(10000)
-x_init   = np.random.normal(loc=0, scale=0.2e-3, size=num_part)
+particle_ref = xp.Particles.reference_from_pdg_id(pdg_id='proton', p0c=6.8e12)
+x_init   = np.random.normal(loc=0.001, scale=0.2e-3, size=num_part)
 px_init  = np.random.normal(loc=0., scale=5.e-6, size=num_part)
 y_init   = np.random.normal(loc=0., scale=1e-3, size=num_part)
 py_init  = np.random.normal(loc=0., scale=5.e-6, size=num_part)
 part_init = xp.build_particles(x=x_init, px=px_init, y=y_init, py=py_init, particle_ref=particle_ref,
                           _capacity=_capacity)
 part = part_init.copy()
+part2 = part_init.copy()
+
+
+# Connect to FLUKA
+xc.FlukaEngine.start(elements=coll, names=coll_name, debug_level=1, _capacity=_capacity)
+xc.FlukaEngine.set_particle_ref(particle_ref=particle_ref)
 
 
 # Do the tracking
 start = time.time()
 coll.track(part)
-print(f"Tracking {num_part} particles took {round(time.time()-start,1)}s")
+print(f"Tracking {num_part} particles (FLUKA) took {round(time.time()-start, 3)}s")
+start = time.time()
+coll2.track(part2)
+print(f"Tracking {num_part} particles (Everest) took {round(time.time()-start, 3)}s")
+
+print(f"Survived in FLUKA: {len(part.state[part.state>0])}/{num_part}")
+print(f"Survived in Everest: {len(part2.state[part2.state>0])}/{num_part}")
 
 
 # Stop the FLUKA server
 xc.FlukaEngine.stop()
 
-mask = part.state > -9999999
+
+# Make some plots
+
+# mask = part.state > -9999999
+mask = part.state > 0
 parents = part.parent_particle_id[mask]
-print(parents)
-part_init.particle_id[parents]
-print(sorted(np.unique((part.x[mask] - part_init.x[parents])/part_init.kin_xprime[parents])))
+# print(sorted(np.unique((part.x[mask] - part_init.x[parents])/part_init.kin_xprime[parents])))
+
+output = part.kin_xprime[mask] - part_init.kin_xprime[parents]
+new_mask = (output > -0.0001) & (output < 0.0001)
+_ = plt.hist2d(part_init.x[parents][new_mask], output[new_mask], 200)
+plt.show()
+
+
+# mask = part.state > -9999999
+mask2 = part2.state > 0
+parents2 = part2.parent_particle_id[mask2]
+# print(sorted(np.unique((part.x[mask] - part_init.x[parents])/part_init.kin_xprime[parents])))
+
+output2 = part2.kin_xprime[mask2] - part_init.kin_xprime[parents2]
+new_mask2 = (output2 > -0.0001) & (output2 < 0.0001)
+_ = plt.hist2d(part_init.x[parents2][new_mask2], output2[new_mask2], 200)
+plt.show()
+
+
+new_mask = new_mask & (part_init.x[parents] >= coll.jaw_L)
+_ = plt.hist2d(part_init.x[parents][new_mask], output[new_mask], 200)
+plt.show()
+
+
+new_mask2 = new_mask2 & (part_init.x[parents2] >= coll2.jaw_L)
+_ = plt.hist2d(part_init.x[parents2][new_mask2], output2[new_mask2], 200)
+plt.show()
