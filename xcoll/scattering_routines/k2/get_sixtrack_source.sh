02f83fcbb742e9d194d44f5a8a6007ef92ad10e3
Frederik F. Van der Veken <frederik@cern.ch>
2024-07-30 09:40:06 +0200

    Added necessary change to get_sixtrack_source.sh

--- xcoll/scattering_routines/k2/get_sixtrack_source.sh
+++ xcoll/scattering_routines/k2/get_sixtrack_source.sh
@@ -41,5 +41,6 @@ sed -i 's/real(kind=fPrec), private, save :: emitny0_dist    = zero/real(kind=fP
 sed -i 's/real(kind=fPrec), private, save :: emitnx0_collgap = zero/real(kind=fPrec), public, save :: emitnx0_collgap = zero/g' collimation.f90
 sed -i 's/real(kind=fPrec), private, save :: emitny0_collgap = zero/real(kind=fPrec), public, save :: emitny0_collgap = zero/g' collimation.f90
 sed -i 's/integer,          allocatable, private, save :: part_hit_pos(:)   /integer,          allocatable, public, save :: part_hit_pos(:)   /g' collimation.f90
+sed -i 's/integer,          allocatable, private, save :: part_hit_turn(:)   /integer,          allocatable, public, save :: part_hit_turn(:)   /g' collimation.f90
 sed -i 's/call shuffleLostParticles/!call shuffleLostParticles/g' collimation.f90
 sed -i 's/if(ithick == 1) call synuthck/!if(ithick == 1) call synuthck/g' mod_particles.f90
