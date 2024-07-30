cf7aa36d32a07130fef0090256b2596413dca29a
Frederik F. Van der Veken <frederik@cern.ch>
2024-07-30 09:51:51 +0200

    Updated get_sixtrack_source.sh

--- xcoll/scattering_routines/k2/get_sixtrack_source.sh
+++ xcoll/scattering_routines/k2/get_sixtrack_source.sh
@@ -35,12 +35,25 @@ do
 done
 mv roundctl_CMakeLists.txt roundctl/CMakeLists.txt
 
-sed -i 's/call coll_getMinGapID(minGapID)/!call coll_getMinGapID(minGapID)/g' collimation.f90
-sed -i 's/real(kind=fPrec), private, save :: emitnx0_dist    = zero/real(kind=fPrec), public, save :: emitnx0_dist    = zero/g' collimation.f90
-sed -i 's/real(kind=fPrec), private, save :: emitny0_dist    = zero/real(kind=fPrec), public, save :: emitny0_dist    = zero/g' collimation.f90
-sed -i 's/real(kind=fPrec), private, save :: emitnx0_collgap = zero/real(kind=fPrec), public, save :: emitnx0_collgap = zero/g' collimation.f90
-sed -i 's/real(kind=fPrec), private, save :: emitny0_collgap = zero/real(kind=fPrec), public, save :: emitny0_collgap = zero/g' collimation.f90
-sed -i 's/integer,          allocatable, private, save :: part_hit_pos(:)   /integer,          allocatable, public, save :: part_hit_pos(:)   /g' collimation.f90
-sed -i 's/integer,          allocatable, private, save :: part_hit_turn(:)   /integer,          allocatable, public, save :: part_hit_turn(:)   /g' collimation.f90
-sed -i 's/call shuffleLostParticles/!call shuffleLostParticles/g' collimation.f90
-sed -i 's/if(ithick == 1) call synuthck/!if(ithick == 1) call synuthck/g' mod_particles.f90
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+    sed -i '' 's/call coll_getMinGapID(minGapID)/!call coll_getMinGapID(minGapID)/g' collimation.f90
+    sed -i '' 's/real(kind=fPrec), private, save :: emitnx0_dist    = zero/real(kind=fPrec), public,  save :: emitnx0_dist    = zero/g' collimation.f90
+    sed -i '' 's/real(kind=fPrec), private, save :: emitny0_dist    = zero/real(kind=fPrec), public,  save :: emitny0_dist    = zero/g' collimation.f90
+    sed -i '' 's/real(kind=fPrec), private, save :: emitnx0_collgap = zero/real(kind=fPrec), public,  save :: emitnx0_collgap = zero/g' collimation.f90
+    sed -i '' 's/real(kind=fPrec), private, save :: emitny0_collgap = zero/real(kind=fPrec), public,  save :: emitny0_collgap = zero/g' collimation.f90
+    sed -i '' 's/integer,          allocatable, private, save :: part_hit_pos(:)   /integer,          allocatable, public,  save :: part_hit_pos(:)   /g' collimation.f90
+    sed -i '' 's/integer,          allocatable, private, save :: part_hit_turn(:)  /integer,          allocatable, public,  save :: part_hit_turn(:)  /g' collimation.f90
+    sed -i '' 's/call shuffleLostParticles/!call shuffleLostParticles/g' collimation.f90
+    sed -i '' 's/if(ithick == 1) call synuthck/!if(ithick == 1) call synuthck/g' mod_particles.f90
+else
+    sed -i 's/call coll_getMinGapID(minGapID)/!call coll_getMinGapID(minGapID)/g' collimation.f90
+    sed -i 's/real(kind=fPrec), private, save :: emitnx0_dist    = zero/real(kind=fPrec), public,  save :: emitnx0_dist    = zero/g' collimation.f90
+    sed -i 's/real(kind=fPrec), private, save :: emitny0_dist    = zero/real(kind=fPrec), public,  save :: emitny0_dist    = zero/g' collimation.f90
+    sed -i 's/real(kind=fPrec), private, save :: emitnx0_collgap = zero/real(kind=fPrec), public,  save :: emitnx0_collgap = zero/g' collimation.f90
+    sed -i 's/real(kind=fPrec), private, save :: emitny0_collgap = zero/real(kind=fPrec), public,  save :: emitny0_collgap = zero/g' collimation.f90
+    sed -i 's/integer,          allocatable, private, save :: part_hit_pos(:)   /integer,          allocatable, public,  save :: part_hit_pos(:)   /g' collimation.f90
+    sed -i 's/integer,          allocatable, private, save :: part_hit_turn(:)  /integer,          allocatable, public,  save :: part_hit_turn(:)  /g' collimation.f90
+    sed -i 's/call shuffleLostParticles/!call shuffleLostParticles/g' collimation.f90
+    sed -i 's/if(ithick == 1) call synuthck/!if(ithick == 1) call synuthck/g' mod_particles.f90
+fi
