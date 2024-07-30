a2c5bcc2411f69b0fc11f6fd46707cd193ac53ba
Frederik F. Van der Veken <frederik@cern.ch>
2024-07-30 09:32:45 +0200

    Made K2 lossmap example more standard

--- examples/_k2_lossmap.py
+++ examples/_k2_lossmap.py
@@ -1,47 +1,81 @@
-import json
+# copyright ############################### #
+# This file is part of the Xcoll Package.   #
+# Copyright (c) CERN, 2024.                 #
+# ######################################### #
+
 import numpy as np
-import pandas as pd
 from pathlib import Path
-import matplotlib.pyplot as plt
+import time
+start_time = time.time()
+
 import xobjects as xo
-import xpart as xp
 import xtrack as xt
 import xcoll as xc
 from xcoll.scattering_routines.k2 import K2Engine
 
-beam          =  1
-plane         = 'V'
-num_turns     = 20
-num_particles = 20000
 
-line = xt.Line.from_json(xc._pkg_root.parent / 'examples' / 'machines' / 'lhc_run3_b1.json')
+beam = 1
+plane = 'H'
 
-colldb = xc.CollimatorDatabase.from_yaml(xc._pkg_root.parent / 'examples' / 'colldb' / 'lhc_run3_crystals.yaml',
-                                         beam=beam, ignore_crystals=False)
+num_turns = 200
+num_particles = 50000
 
+path_in  = xc._pkg_root.parent / 'examples'
+path_out = Path.cwd()
+
+
+# Load from json
+line = xt.Line.from_json(path_in / 'machines' / f'lhc_run3_b{beam}.json')
+
+
+# Initialise colldb
+colldb = xc.CollimatorDatabase.from_yaml(path_in / 'colldb' / f'lhc_run3.yaml', beam=beam)
+
+
+# Install collimators into line
 colldb._install_k2_collimators(line=line, verbose=True)
 
+
+# Aperture model check
+print('\nAperture model check after introducing collimators:')
+df_with_coll = line.check_aperture()
+assert not np.any(df_with_coll.has_aperture_problem)
+
+
+# Build the tracker
 line.build_tracker()
 
+
+# Assign the optics to deduce the gap settings
 xc.assign_optics_to_collimators(line=line)
 
+
+# Start the engine
 K2Engine.start(line=line, cwd='run_1')
 
-part_init = xc.generate_pencil_on_collimator(line, 'tcp.d6l7.b1', num_particles, side='+')
-#part_init = line.build_particles(x=np.ones(num_particles)*1e-3)
-part = part_init.copy()
 
-xc.enable_scattering(line=line)
-
-print(np.unique(part.s))
-line['tcp.d6l7.b1'].track(part)
-print(np.unique(part.s))
-print(np.unique(part.state))
+# Generate initial pencil distribution on horizontal collimator
+tcp  = f"tcp.{'c' if plane=='H' else 'd'}6{'l' if f'{beam}'=='1' else 'r'}7.b{beam}"
+part = xc.generate_pencil_on_collimator(line, tcp, num_particles=num_particles)
 
 
-dri = xt.Drift(length=line['tcp.d6l7.b1'].length)
-part2 = part_init.copy()
-dri.track(part2)
 
-plt.plot(part_init.kin_yprime, part.kin_yprime - part2.kin_yprime, 'o', markersize=1)
-plt.show()
+# Track!
+xc.enable_scattering(line)
+line.track(part, num_turns=num_turns, time=True, with_progress=1)
+xc.disable_scattering(line)
+print(f"Done tracking in {line.time_last_track:.1f}s.")
+
+
+# Save lossmap to json, which can be loaded, combined (for more statistics),
+# and plotted with the 'lossmaps' package
+line_is_reversed = True if f'{beam}' == '2' else False
+ThisLM = xc.LossMap(line, line_is_reversed=line_is_reversed, part=part)
+ThisLM.to_json(file=Path(path_out, f'lossmap_B{beam}{plane}.json'))
+
+# Save a summary of the collimator losses to a text file
+ThisLM.save_summary(file=Path(path_out, f'coll_summary_B{beam}{plane}.out'))
+print(ThisLM.summary)
+
+print(f"Total calculation time {time.time()-start_time}s")
+
