25a6c1eb5eebc0ef1c0db818db718f2a4095aac1
Frederik F. Van der Veken <frederik@cern.ch>
2024-10-07 12:19:34 +0200

    Reactivated all geometry tests

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -16,7 +16,7 @@ from xcoll_geometry_test import XcollGeometryTest
 
 def _init_kernels():
     geom = XcollGeometryTest()
-    geom.compile_kernels(only_if_needed=True, save_source_as='geometry.c')
+    geom.compile_kernels(only_if_needed=True) #, save_source_as='geometry.c')
     return geom._context.kernels
 
 def _generate_polygon_points(num_poly, tilt_L=0, tilt_R=0):
@@ -191,19 +191,19 @@ def test_jaw_after_s():
                                         s_D=s_poly[2], x_D=x_poly[2], tilt_tan=tilt_tan, side=side, after_s=0.6)
     _loop_2jaw_1partdim(name='expected_s_jaw_after_s', func=func, num_polys=4)
 
-# def test_jaw_vlimit_first():
-#     def func(part_x, part_tan_x, part_y, part_tan_y, s_poly, x_poly, tilt_tan, side):
-#         return kernels.test_jaw_vlimit_first(part_s=0, part_x=part_x, part_tan_x=part_tan_x, part_y=part_y, part_tan_y=part_tan_y,
-#                                             s_U=s_poly[1], x_U=x_poly[1], s_D=s_poly[2], x_D=x_poly[2], tilt_tan=tilt_tan,
-#                                             side=side, y_min=-0.1, y_max=0.25)
-#     _loop_doublejaw_2partdim(name='expected_s_jaw_with_vlimit', func=func, num_polys=4)
+def test_jaw_vlimit_first():
+    def func(part_x, part_tan_x, part_y, part_tan_y, s_poly, x_poly, tilt_tan, side):
+        return kernels.test_jaw_vlimit_first(part_s=0, part_x=part_x, part_tan_x=part_tan_x, part_y=part_y, part_tan_y=part_tan_y,
+                                            s_U=s_poly[1], x_U=x_poly[1], s_D=s_poly[2], x_D=x_poly[2], tilt_tan=tilt_tan,
+                                            side=side, y_min=-0.1, y_max=0.25)
+    _loop_doublejaw_2partdim(name='expected_s_jaw_with_vlimit', func=func, num_polys=4)
 
-# def test_jaw_vlimit_after_s():
-#     def func(part_x, part_tan_x, part_y, part_tan_y, s_poly, x_poly, tilt_tan, side):
-#         return kernels.test_jaw_vlimit_after_s(part_s=0, part_x=part_x, part_tan_x=part_tan_x, part_y=part_y, part_tan_y=part_tan_y,
-#                                             s_U=s_poly[1], x_U=x_poly[1], s_D=s_poly[2], x_D=x_poly[2], tilt_tan=tilt_tan,
-#                                             side=side, y_min=-0.1, y_max=0.25, after_s=0.6)
-#     _loop_doublejaw_2partdim(name='expected_s_jaw_after_s_with_vlimit', func=func, num_polys=4)
+def test_jaw_vlimit_after_s():
+    def func(part_x, part_tan_x, part_y, part_tan_y, s_poly, x_poly, tilt_tan, side):
+        return kernels.test_jaw_vlimit_after_s(part_s=0, part_x=part_x, part_tan_x=part_tan_x, part_y=part_y, part_tan_y=part_tan_y,
+                                            s_U=s_poly[1], x_U=x_poly[1], s_D=s_poly[2], x_D=x_poly[2], tilt_tan=tilt_tan,
+                                            side=side, y_min=-0.1, y_max=0.25, after_s=0.6)
+    _loop_doublejaw_2partdim(name='expected_s_jaw_after_s_with_vlimit', func=func, num_polys=4)
 
 def test_polygon_first():
     def func(part_x, part_tan_x, part_y, part_tan_y, s_poly, x_poly, tilt_tan, side):
