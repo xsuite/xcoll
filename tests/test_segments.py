b0f365bed5974e9f613202947fc4d6460eaa1acd
Frederik Van der Veken <frederik@cern.ch>
2024-10-20 01:42:02 +0200

    First steps towards segments tests - WIP.
    Co-authored-by: Simone Solstrand <simonesol@hotmail.no>

--- tests/test_segments.py
+++ tests/test_segments.py
@@ -11,9 +11,8 @@ from pathlib import Path
 
 import xobjects as xo
 import xcoll as xc
-from xcoll.scattering_routines.geometry.segments import *
-from xcoll.scattering_routines.geometry.c_init import *
-from xcoll.scattering_routines.geometry.segments.trajectories import get_max_crossings
+
+from xcoll.scattering_routines.geometry.trajectories import get_max_crossings
 from xobjects.test_helpers import for_all_test_contexts
 
 path = Path(__file__).parent / 'data_test_segments'
@@ -24,7 +23,7 @@ context.add_kernels(
         "Segments_crossing_drift": xo.Kernel(
             c_name="Segments_crossing_drift",
             args=[
-                xo.Arg(Segments,   name="segs"),
+                xo.Arg(xcSegments, name="segs"),
                 xo.Arg(xo.Int8,    pointer=True, name="n_hit"),
                 xo.Arg(xo.Float64, pointer=True,  name="s"),
                 xo.Arg(xo.Float64, pointer=False, name="s0"),
@@ -38,24 +37,24 @@ context.add_kernels(
 
 
 # Segment(s) to be tested #############################
-line_segment      = LineSegment(s1=0,x1=0,s2=0.2,x2=1)
-half_open_segment = HalfOpenLineSegment(s=2, x=1, t=np.pi/4)
-bezier_segment    = BezierSegment(s1=0.2, x1=1, s2=1+0.5*np.cos(3*np.pi/4), x2=0.5+0.5*np.sin(3*np.pi/4), cs1=0.5, cx1=2.5, cs2=1+0.5*np.cos(3*np.pi/4)-0.6, cx2=0.5+0.5*np.sin(3*np.pi/4)-0.6)
+line_segment      = xc.LineSegment(s1=0,x1=0,s2=0.2,x2=1)
+half_open_segment = xc.HalfOpenLineSegment(s=2, x=1, t=np.pi/4)
+bezier_segment    = xc.BezierSegment(s1=0.2, x1=1, s2=1+0.5*np.cos(3*np.pi/4), x2=0.5+0.5*np.sin(3*np.pi/4), cs1=0.5, cx1=2.5, cs2=1+0.5*np.cos(3*np.pi/4)-0.6, cx2=0.5+0.5*np.sin(3*np.pi/4)-0.6)
 
-half_circle       = CircularSegment(R=0.5, s=1, x=0.5, t1=-np.pi/2, t2=np.pi/2)
-half_circle_2     = CircularSegment(R=0.5, s=1, x=0.5, t1=np.pi/2, t2=3*np.pi/2)
-half_circle_3     = CircularSegment(R=0.5, s=1, x=0.5, t1=0, t2=np.pi)
-half_circle_4     = CircularSegment(R=0.5, s=1, x=0.5, t1=np.pi, t2=2*np.pi)
+half_circle       = xc.CircularSegment(R=0.5, s=1, x=0.5, t1=-np.pi/2, t2=np.pi/2)
+half_circle_2     = xc.CircularSegment(R=0.5, s=1, x=0.5, t1=np.pi/2, t2=3*np.pi/2)
+half_circle_3     = xc.CircularSegment(R=0.5, s=1, x=0.5, t1=0, t2=np.pi)
+half_circle_4     = xc.CircularSegment(R=0.5, s=1, x=0.5, t1=np.pi, t2=2*np.pi)
 
-seg        = Segments(data=[LineSegment(s1=0, x1=0, s2=0.2, x2=1),
-                            BezierSegment(s1=0.2, x1=1, s2=1+0.5*np.cos(3*np.pi/4), x2=0.5+0.5*np.sin(3*np.pi/4), cs1=0.5, cx1=2.5, cs2=1+0.5*np.cos(3*np.pi/4)-0.6, cx2=0.5+0.5*np.sin(3*np.pi/4)-0.6),
-                            CircularSegment(R=0.5, s=1, x=0.5, t1=-np.pi/2, t2=3*np.pi/4),
-                            LineSegment(s1=1, x1=0, s2=0, x2=0)])
+seg        = xc.Segments([xc.LineSegment(s1=0, x1=0, s2=0.2, x2=1),
+                          xc.BezierSegment(s1=0.2, x1=1, s2=1+0.5*np.cos(3*np.pi/4), x2=0.5+0.5*np.sin(3*np.pi/4), cs1=0.5, cx1=2.5, cs2=1+0.5*np.cos(3*np.pi/4)-0.6, cx2=0.5+0.5*np.sin(3*np.pi/4)-0.6),
+                          xc.CircularSegment(R=0.5, s=1, x=0.5, t1=-np.pi/2, t2=3*np.pi/4),
+                          xc.LineSegment(s1=1, x1=0, s2=0, x2=0)])
 
-seg_bezier = Segments(data=[BezierSegment(s1=0.2, x1=1, s2=1+0.5*np.cos(3*np.pi/4), x2=0.5+0.5*np.sin(3*np.pi/4), cs1=0.5, cx1=2.5, cs2=1+0.5*np.cos(3*np.pi/4)-0.6, cx2=0.5+0.5*np.sin(3*np.pi/4)-0.6),
-                            BezierSegment(s1=1+0.5*np.cos(3*np.pi/4), x1=0.5+0.5*np.sin(3*np.pi/4), s2=1.2, x2=0.5, cs1=1.4, cx1=1.6, cs2=1.1, cx2=0.9),
-                            BezierSegment(s1=1.2, x1=0.5, s2=0.5, x2=0.2, cs1=1.2, cx1=0.1, cs2=0.4, cx2=0.1),
-                            BezierSegment(s1=0.5, x1=0.2, s2=0.2, x2=1, cs1=0.7, cx1=0.5, cs2=0.1, cx2=0.2)])
+seg_bezier = xc.Segments([xc.BezierSegment(s1=0.2, x1=1, s2=1+0.5*np.cos(3*np.pi/4), x2=0.5+0.5*np.sin(3*np.pi/4), cs1=0.5, cx1=2.5, cs2=1+0.5*np.cos(3*np.pi/4)-0.6, cx2=0.5+0.5*np.sin(3*np.pi/4)-0.6),
+                          xc.BezierSegment(s1=1+0.5*np.cos(3*np.pi/4), x1=0.5+0.5*np.sin(3*np.pi/4), s2=1.2, x2=0.5, cs1=1.4, cx1=1.6, cs2=1.1, cx2=0.9),
+                          xc.BezierSegment(s1=1.2, x1=0.5, s2=0.5, x2=0.2, cs1=1.2, cx1=0.1, cs2=0.4, cx2=0.1),
+                          xc.BezierSegment(s1=0.5, x1=0.2, s2=0.2, x2=1, cs1=0.7, cx1=0.5, cs2=0.1, cx2=0.2)])
 
 # Test functions #####################################
 def test_line_segment(): 
@@ -80,20 +79,16 @@ def test_segments_bezier():
     _check_ref('bezier_segments', seg_bezier)
 
 def _check_ref(name, seg, _context=None):
-    print(f"Testing {name}")
-    # if _context is None:
-    #     _context = xo.ContextCpu()
     with open(Path(path, 'ref_'+name+'.json'), 'r') as fid:
         ref = json.load(fid)
-    print('yes')
     ref_s     = np.array(ref['hit_s'])
     ref_x     = np.array(ref['hit_x'])
     ref_m     = np.array(ref['m'])
     ref_point = np.array(ref['point'])
-    print('yes')
+
     n_hit = np.zeros(1, dtype=np.int8)
     s = np.zeros(get_max_crossings(seg, "drift"), dtype=np.float64)
-    print('yes')
+
     s0, x0 = ref_point
     n_hit[0] = 0
     hits_s = np.array([], dtype=np.float64)
@@ -106,10 +101,3 @@ def _check_ref(name, seg, _context=None):
     assert np.allclose(hits_s, ref_s, atol=1e-12, rtol=0), "s does not match."
     assert np.allclose(hits_x, ref_x, atol=1e-12, rtol=0), "x does not match"
     print(f"Test for {name} passed.")
-
-test_line_segment()
-test_half_open_segment()
-test_bezier_segment()
-test_half_circle()
-test_segments()
-test_segments_bezier()
