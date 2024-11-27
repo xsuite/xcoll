Adapted yaml for tests: tilt was too big (and wrong sign) to keep TCP.H converging

xcoll/beam_elements/base.py
@@ -12,8 +12,8 @@ from ..interaction_record import InteractionRecord
 from ..general import _pkg_root
 
 
-OPEN_JAW = 3.
-OPEN_GAP = 999.
+OPEN_JAW = 3
+OPEN_GAP = 999
 
 
 class InvalidXcoll(xt.BeamElement):
@@ -744,8 +744,8 @@ class BaseCollimator(BaseBlock):
             alfy = self.optics[self.align]['alfy'][0]
             betx = self.optics[self.align]['betx'][0]
             bety = self.optics[self.align]['bety'][0]
-            divx = -np.sqrt(self.nemitt_x/self.optics['beta_gamma_rel']/betx)*alfx
-            divy = -np.sqrt(self.nemitt_y/self.optics['beta_gamma_rel']/bety)*alfy
+            divx = -np.sqrt(self.gemitt_x/betx)*alfx
+            divy = -np.sqrt(self.gemitt_y/bety)*alfy
             if hasattr(self, '_cos_zL'):
                 if self.side != 'right':
                     return divx if abs(self.angle_L) < 1e-6 else divy

xcoll/initial_distribution.py
@@ -63,8 +63,7 @@ def generate_pencil_on_collimator(line, name, num_particles, *, side='+-', penci
     # TODO: skew collimators
     tilt = coll.tilt[0] if isinstance(coll.tilt, list) else coll.tilt
     gemitts = {'x': coll.gemitt_x, 'y': coll.gemitt_y}
-    betatron_angle = -coll.gap * twiss[f'alf{plane}', name] * np.sqrt(gemitts[plane] \
-                     / twiss[f'bet{plane}', name])
+    betatron_angle = -coll.gap * coll.divergence
     tolerance_tilt = 1e-12 # 0.1 urad tolerance on jaw tilt  =>  we prioritise converging
     is_converging = tilt + tolerance_tilt >= betatron_angle
     print(f"Collimator {name} is {'con' if is_converging else 'di'}verging.")