00884929a39370daa828b9e39e0e3e7639bf24f7
Simone Solstrand <simonesol@hotmail.no>
2025-02-03 15:40:34 +0100

    Change func and deriv for segments

--- xcoll/geometry/segments/halfopen_line.h
+++ xcoll/geometry/segments/halfopen_line.h
@@ -8,6 +8,32 @@
 #define XC_HALFOPENLINE_CROSSINGS 2
 
 
+
+/*gpufun*/
+double HalfOpenLineSegment_func(HalfOpenLineSegment seg, double s){
+    // MCS trajectory form PDG rewritted in terms of A, B and s/Xo. 
+    const double Ax = HalfOpenLine_get_Ax(seg);
+    const double Xo = HalfOpenLine_get_Xo(seg);
+    double s2       = HalfOpenLine_get_s2(seg);
+    double x2       = HalfOpenLine_get_x2(seg);
+    double s1       = HalfOpenLine_get_s1(seg);
+    double x1       = HalfOpenLine_get_x1(seg);
+    return x2 + (x2 - x1) / (s2 - s1) * (s - s1);
+}
+
+/*gpufun*/
+double HalfOpenLineSegment_deriv(HalfOpenLineSegment seg, double s){
+    // MCS trajectory derivative wrt s
+    const double Ax = HalfOpenLine_get_Ax(seg);
+    const double Xo = HalfOpenLine_get_Xo(seg);
+    double s2       = HalfOpenLine_get_s2(seg);
+    double x2       = HalfOpenLine_get_x2(seg);
+    double s1       = HalfOpenLine_get_s1(seg);
+    double x1       = HalfOpenLine_get_x1(seg);
+    return (x2 - x1) / (s2 - s1);
+}
+
+
 /*gpufun*/
 void HalfOpenLineSegment_crossing_drift(HalfOpenLineSegment seg, int8_t* n_hit, double* s, double s0, double x0, double xm){
     // Get segment data
@@ -40,58 +66,7 @@ void HalfOpenLineSegment_crossing_drift(HalfOpenLineSegment seg, int8_t* n_hit, 
 }
 
 /*gpufun*/
-double MultipleCoulomb_HalfOpenLine(double s, McsHalfOpenLineParams params){
-    // MCS trajectory form PDG rewritted in terms of A, B and s/Xo. 
-    const double Ax = McsHalfOpenLineParams_get_Ax(params);
-    const double Xo = McsHalfOpenLineParams_get_Xo(params);
-    double s2       = McsHalfOpenLineParams_get_s2(params);
-    double x2       = McsHalfOpenLineParams_get_x2(params);
-    double s1       = McsHalfOpenLineParams_get_s1(params);
-    double x1       = McsHalfOpenLineParams_get_x1(params);
-
-    double mcs  = Ax * pow(sqrt(s/Xo),3.0) * (1.0/0.038 + log(s/Xo));
-    double half_open_line = x2 + (x2 - x1) / (s2 - s1) * (s - s1);
-    return mcs - half_open_line;
-}
-
-/*gpufun*/
-double MultipleCoulombDeriv_HalfOpenLine(double s,  McsHalfOpenLineParams params){
-    // MCS trajectory derivative wrt s
-    const double Ax = McsHalfOpenLineParams_get_Ax(params);
-    const double Xo = McsHalfOpenLineParams_get_Xo(params);
-    double s2       = McsHalfOpenLineParams_get_s2(params);
-    double x2       = McsHalfOpenLineParams_get_x2(params);
-    double s1       = McsHalfOpenLineParams_get_s1(params);
-    double x1       = McsHalfOpenLineParams_get_x1(params);
-
-    double mcs_deriv  = Ax/Xo * (sqrt(s/Xo)*3.0/2.0*log(s/Xo)+1.0/0.038 + sqrt(s/Xo));
-    double half_open_line_deriv = (x2 - x1) / (s2 - s1);
-    return mcs_deriv - half_open_line_deriv;
-}
-
-/*gpufun*/
 void HalfOpenLineSegment_crossing_mcs(HalfOpenLineSegment seg, int8_t* n_hit, double* s, const double* Ax, const double Xo){
-    // // Get segment data
-    // double s1 = HalfOpenLineSegment_get_s(seg);
-    // double x1 = HalfOpenLineSegment_get_x(seg);
-    // double s2 = s1 + cos(HalfOpenLineSegment_get_t(seg));
-    // double x2 = x1 + sin(HalfOpenLineSegment_get_t(seg));
-
-    // // Define roots array and parameters
-    // double roots[XC_HALFOPENLINE_CROSSINGS];
-    // int number_of_roots = 0;
-    // Params_HalfOpenLine params = {Xo, *Ax, x2, s2, x1, s1};
-
-    // grid_search_and_newton(MultipleCoulomb_HalfOpenLine, MultipleCoulombDeriv_HalfOpenLine, s1, s2, roots, XC_HALFOPENLINE_CROSSINGS, &params, &number_of_roots);
-    // // now we have the roots, but that doesnt mean anything yet
-    
-    // // should this function be about finding the crossings, and THEN checking in jaw if nucl or not, and then updating nhit?
-    // // Process the roots
-    // for (int i = 0; i < XC_HALFOPENLINE_CROSSINGS; ++i) {
-    //     if (roots[i] >= s1 && roots[i] <= s2 && i < number_of_roots){
-    //         s[*n_hit] = roots[i];
-    //         (*n_hit)++;
-    //     }
-    // }
+   return grid_search_and_newton()
 }  
 #endif /* XCOLL_COLL_GEOM_HALFOPENLINESEG_H */
