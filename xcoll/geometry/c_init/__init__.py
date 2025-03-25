91dbd5297c59e8475b7a5874bcec276588ac59a2
simoneotelie <simoneos@stud.ntnu.no>
2025-03-25 16:21:08 +0100

    Removed Geom

--- xcoll/geometry/c_init/__init__.py
+++ xcoll/geometry/c_init/__init__.py
@@ -3,4 +3,4 @@
 # Copyright (c) CERN, 2025.                 #
 # ######################################### #
 
-from .c_init import GeomCInit, XC_GEOM_EPSILON, XC_GEOM_S_MAX, xo_to_ctypes, xo_to_cnames, PyMethod
+from .c_init import BoundingBox, XC_GEOM_EPSILON, XC_GEOM_S_MAX, xo_to_ctypes, xo_to_cnames, PyMethod
