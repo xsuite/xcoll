c08944e8677d326ac62dd79327a3d40a132ed1dd
Frederik F. Van der Veken <frederik@cern.ch>
2025-06-18 22:35:25 +0200

    Made better line accessor.

--- xcoll/colldb.py
+++ xcoll/colldb.py
@@ -550,6 +550,10 @@ class CollimatorDatabase:
         prop_dict.update(kwargs)
         el = cls(**prop_dict)
         el.emittance = [self.nemitt_x, self.nemitt_y]
+        if 'family' in self[name] and self[name]['family'].lower() != 'unknown':
+            if self[name]['family'] == name:
+                raise ValueError(f"Collimator {name} has the same name as its family!")
+            el.family = self[name]['family']
         self._elements[name] = el
 
     def install_black_absorbers(self, line, *, names=None, families=None, verbose=False, need_apertures=True):
