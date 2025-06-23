028462eb3e5dd958e31f754b3bf4d534a329657b
Frederik F. Van der Veken <frederik@cern.ch>
2025-06-24 00:47:42 +0200

    More explanation in release.py

--- release.py
+++ release.py
@@ -11,12 +11,16 @@
 #     - Make sure you have release rights for the package on PyPI and GitHub
 #     - Install xaux and poetry (pip install xaux poetry)
 #     - Install gh (GitHub Command Line interface, see https://cli.github.com/)
-#     - Get an API token from PYPI (https://pypi.org/manage/account/ -> "Add API token")
+#     - Get an API token from PYPI (https://pypi.org/manage/account/ -> "Add API token"). Make
+#       sure the package is covere by the scope of the token.
 #     - Set the PyPI token with poetry: `poetry config pypi-token.pypi <my-token>`
-#     - Get an API token from GitHub (github.com -> profile -> Settings -> Developer Settings -> Personal access tokens -> Tokens (classic)
+#     - Get an API token from GitHub (github.com -> profile -> "Settings" -> "Developer Settings"
+#       -> "Personal access tokens" -> "Tokens (classic)"). You will need all scopes under "repo",
+#       "admin:org", "admin:public_key", and "admin:repo_hook".
 #     - Set the GitHub token with gh: `gh auth login --with-token` and then paste the token.
-#       In principle, this should be done for every terminal session, but an easy way around is to store the token in a
-#       private file (not exposed to the network) and add `gh auth login --with-token <  <path_to_file>` to your .bashrc
+#       In principle, this should be done for every terminal session, but an easy way around is to
+#       store the token in a private file (not exposed to the network) and add
+#       `gh auth login --with-token <  <path_to_file>` to your .bashrc
 #
 # Steps:
 #     - Version numbers are of the form major.minor.patch
