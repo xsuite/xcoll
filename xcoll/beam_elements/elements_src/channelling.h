03e0e310faafa6d1ef5cf2bd31e618a8bfb7bf66
Anastasia Iliopoulou <Anastasia.Iliopoulou@cern.ch>
2025-08-11 16:35:42 +0200

    Moved Et as an argument instead of recalculating it every time.

--- xcoll/beam_elements/elements_src/channelling.h
+++ xcoll/beam_elements/elements_src/channelling.h
@@ -16,48 +16,45 @@ static inline double E_T_simplemoliere(double x, double theta, double bpc, doubl
     return bpc*theta*theta / 2.0+U_N*(cosh(beta_i/a_TF*x) - 1.0);
 }
 
-static inline double m1_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF) {
-    double E_T = E_T_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
+static inline double m1_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T ) {
     return 1.0 + 2.0*U_N /E_T;
 }
 
-static inline double m1p_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF) {
-    double E_T = E_T_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
+static inline double m1p_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T ) {
     return E_T/(E_T + 2.0*U_N);
 }
 
-static inline double nu_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF) {
-    double E_T = E_T_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
+static inline double nu_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T) {
     return -copysign(1.0,theta)*beta_i/a_TF*sqrt(E_T/(2.0*bpc));
 }
 
-static inline double phi_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF) {
-    double m1 = m1_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
-    double m1p = m1p_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
-    double arg_am = sqrt(m1) * tanh(x * beta_i / (2.0 * a_TF));
-    if (arg_am > 1.0) arg_am = 1.0 - 1e-14;
-    if (arg_am < -1.0) arg_am = -1.0 + 1e-14;
-    double phi_in = asin(arg_am);
-    double F = ellik(phi_in, m1p);
-    return -sqrt(m1p) * F;
+static inline double phi_simplemoliere(double x, double theta,
+                                       double bpc, double U_N,
+                                       double beta_i, double a_TF,double E_T) {
+    double arg = - (beta_i * x) / (2.0 * a_TF);
+    double phi_amplitude = atan(sinh(arg));
+    double m = E_T / (E_T + 2.0 * U_N);
+
+    return ellik(phi_amplitude, m);
 }
 
-static inline double x_simplemoliere(double z, double x, double theta, double bpc, double U_N, double beta_i, double a_TF) {
-    double m1 = m1_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
-    double m1p = m1p_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
-    double nu = nu_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
-    double phi = phi_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
+
+static inline double x_simplemoliere(double z, double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T) {
+    double m1 = m1_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
+    double m1p = m1p_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
+    double nu = nu_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
+    double phi = phi_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
     double u = sqrt(m1)*(nu*z + phi);
     double sn, cn, dn, am;
     ellpj(u, m1p, &sn, &cn, &dn, &am);
     return -2.0 * a_TF / beta_i * asinh(sqrt(m1p) * sn / dn);
 }
 
-static inline double theta_simplemoliere(double z, double x, double theta, double bpc, double U_N, double beta_i, double a_TF) {
-    double m1 = m1_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
-    double m1p = m1p_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
-    double nu = nu_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
-    double phi = phi_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF);
+static inline double theta_simplemoliere(double z, double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T) {
+    double m1 = m1_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
+    double m1p = m1p_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
+    double nu = nu_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
+    double phi = phi_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
     double u = sqrt(m1) * (nu * z + phi);
     double sn, cn, dn, am;
     ellpj(u, m1p, &sn, &cn, &dn, &am);
@@ -75,9 +72,9 @@ void ChannellingDev_track_local_particle(ChannellingDevData el, LocalParticle* p
         double x0 = LocalParticle_get_x(part);
         double theta0 = LocalParticle_get_xp(part);
         double z = length;
-
-        double x = x_simplemoliere(z, x0, theta0, bpc, U_N, beta_i, a_TF);
-        double theta = theta_simplemoliere(z, x0, theta0, bpc, U_N, beta_i, a_TF);
+        double E_T=E_T_simplemoliere(x0,  theta0,  bpc,  U_N,  beta_i,  a_TF);
+        double x = x_simplemoliere(z, x0, theta0, bpc, U_N, beta_i, a_TF, E_T);
+        double theta = theta_simplemoliere(z, x0, theta0, bpc, U_N, beta_i, a_TF, E_T);
 
         LocalParticle_set_x(part, x);
         LocalParticle_set_xp(part, theta);
