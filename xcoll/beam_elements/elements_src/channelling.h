4c018b17e01e8796f62ac261156a49f482684bfb
Frederik Van der Veken <frederik@cern.ch>
2025-08-12 13:01:52 +0200

    Some changes

--- xcoll/beam_elements/elements_src/channelling.h
+++ xcoll/beam_elements/elements_src/channelling.h
@@ -12,50 +12,76 @@
 
 // Moliere functions 
 
-static inline double E_T_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF) {
+/*gpufun*/
+double E_T_simplemoliere(double x, double theta, double bpc, double U_N,
+                         double beta_i, double a_TF) {
     return bpc*theta*theta / 2.0+U_N*(cosh(beta_i/a_TF*x) - 1.0);
 }
 
-static inline double m1_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T ) {
+/*gpufun*/
+double m1_simplemoliere(double x, double theta, double bpc, double U_N,
+                        double beta_i, double a_TF, double E_T ) {
     return 1.0 + 2.0*U_N /E_T;
 }
 
-static inline double m1p_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T ) {
+/*gpufun*/
+double m1p_simplemoliere(double x, double theta, double bpc, double U_N,
+                         double beta_i, double a_TF, double E_T ) {
     return E_T/(E_T + 2.0*U_N);
 }
 
-static inline double nu_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T) {
-    return -copysign(1.0,theta)*beta_i/a_TF*sqrt(E_T/(2.0*bpc));
+/*gpufun*/
+double nu_simplemoliere(double x, double theta, double bpc, double U_N,
+                        double beta_i, double a_TF, double E_T) {
+    double sign = -1;
+    if (theta < -1e-12) {
+        sign = 1;
+    }
+    return sign*beta_i/a_TF*sqrt(E_T/(2.0*bpc));
 }
 
-static inline double phi_simplemoliere(double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T) {
+/*gpufun*/
+double phi_simplemoliere(double x, double theta, double nu, double bpc, double U_N,
+                         double beta_i, double a_TF, double E_T) {
     double m = m1_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
     double mp = m1p_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
-    double alpha = 1/sqrt(m*pow(sinh(x*beta_i / (2*a_TF)),2) + pow(theta*beta_i /(2*a_TF),2 ));
+    double sign = -1;
+    if (x < -1e-12) {
+        sign = 1;
+    }
+    if (fabs(theta) < 1e-12) {
+        // If theta is very close to 0, we can avoid numerical issues with asin(1)
+        return sign*sqrt(mp)*ellpk(mp);
+    }
+    double alpha = 1/sqrt(m*pow(sinh(x*beta_i / (2*a_TF)), 2.) + pow(theta*beta_i /(2*a_TF*nu), 2.));
     double phi_amplitude = asin(sqrt((1 - alpha*alpha)/mp));
-    return ellik(phi_amplitude, mp);
+    if (sqrt((1 - alpha*alpha)/mp) > 1) { printf("yes");} else { printf("no");}
+    return sign*sqrt(mp)*ellik(phi_amplitude, mp);
 }
 
 
-static inline double x_simplemoliere(double z, double x, double theta, double bpc, double U_N, double beta_i, double a_TF,double E_T, double phi) {
+/*gpufun*/
+double x_simplemoliere(double z, double x, double theta, double nu, double bpc, double U_N,
+                       double beta_i, double a_TF,double E_T, double phi) {
     double m1 = m1_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
     double m1p = m1p_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
-    double nu = nu_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
     double u = sqrt(m1)*(nu*z + phi);
     double sn, cn, dn, am;
     ellpj(u, m1p, &sn, &cn, &dn, &am);
     return -2.0 * a_TF / beta_i * asinh(sqrt(m1p) * sn / dn);
 }
 
-static inline double theta_simplemoliere(double z, double x, double theta, double bpc, double U_N, double beta_i, double a_TF, double E_T, double phi) {
+/*gpufun*/
+double theta_simplemoliere(double z, double x, double theta, double nu, double bpc, double U_N,
+                           double beta_i, double a_TF, double E_T, double phi) {
     double m1 = m1_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
     double m1p = m1p_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
-    double nu = nu_simplemoliere(x, theta, bpc, U_N, beta_i, a_TF, E_T);
     double u = sqrt(m1) * (nu * z + phi);
     double sn, cn, dn, am;
     ellpj(u, m1p, &sn, &cn, &dn, &am);
     return -2.0 * a_TF / beta_i * nu * cn / dn;
 }
+
 /*gpufun*/
 void ChannellingDev_track_local_particle(ChannellingDevData el, LocalParticle* part0) {
     double length = ChannellingDevData_get_length(el);
@@ -68,13 +94,15 @@ void ChannellingDev_track_local_particle(ChannellingDevData el, LocalParticle* p
         double x0 = LocalParticle_get_x(part);
         double theta0 = LocalParticle_get_xp(part);
         double z = length;
-        double E_T=E_T_simplemoliere(x0,  theta0,  bpc,  U_N,  beta_i,  a_TF);
-        double phi = phi_simplemoliere(x0,  theta0,  bpc,  U_N,  beta_i,  a_TF, E_T);
-        double x = x_simplemoliere(z, x0, theta0, bpc, U_N, beta_i, a_TF, E_T, phi);
-        double theta = theta_simplemoliere(z, x0, theta0, bpc, U_N, beta_i, a_TF, E_T, phi);
+        double E_T = E_T_simplemoliere(x0,  theta0,  bpc,  U_N,  beta_i,  a_TF);
+        double nu = nu_simplemoliere(x0, theta0, bpc, U_N, beta_i, a_TF, E_T);
+        double phi = phi_simplemoliere(x0,  theta0, nu,  bpc,  U_N,  beta_i,  a_TF, E_T);
+        double x = x_simplemoliere(z, x0, theta0, nu, bpc, U_N, beta_i, a_TF, E_T, phi);
+        double theta = theta_simplemoliere(z, x0, theta0, nu, bpc, U_N, beta_i, a_TF, E_T, phi);
 
         LocalParticle_set_x(part, x);
         LocalParticle_set_xp(part, theta);
+        printf("ChannellingDev: x = %f, theta = %f, ET_before = %f, ET_after = %f\n", x*1.e10, theta*1.e9, E_T, E_T_simplemoliere(x,  theta,  bpc,  U_N,  beta_i,  a_TF));
     //end_per_particle_block
 }
 
