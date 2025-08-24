9716e72468d477a59d787810db49d8c7f1ad84a2
Frederik F. Van der Veken <frederik@cern.ch>
2025-08-24 22:06:31 +0200

    Moved prebuild_kernels for xcoll elements into Xcoll.

---
+++ xcoll/prebuild_kernels/__init__.py
@@ -0,0 +1,7 @@
+# copyright ############################### #
+# This file is part of the Xcoll package.   #
+# Copyright (c) CERN, 2025.                 #
+# ######################################### #
+
+from .kernel_definitions import DEFAULT_XCOLL_ELEMENTS
+from .prebuild_kernels import XCOLL_ELEMENTS_INIT_DEFAULTS
---
+++ xcoll/prebuild_kernels/kernel_definitions.py
@@ -0,0 +1,19 @@
+# copyright ############################### #
+# This file is part of the Xcoll package.   #
+# Copyright (c) CERN, 2025.                 #
+# ######################################### #
+
+from ..beam_elements import *
+
+DEFAULT_XCOLL_ELEMENTS = [
+    BlackAbsorber,
+    BlackCrystal,
+    TransparentCollimator,
+    TransparentCrystal,
+    EverestBlock,
+    EverestCollimator,
+    EverestCrystal,
+    BlowUp,
+    ParticleStatsMonitor,
+    EmittanceMonitor
+]
