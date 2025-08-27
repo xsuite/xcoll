516a62047a57936fac83139c84c75070f8a5057e
Frederik Van der Veken <frederik@cern.ch>
2025-08-27 11:54:42 +0200

    Added template for bent_channelling.h

---
+++ xcoll/beam_elements/elements_src/bent_channelling.h
@@ -0,0 +1,13 @@
+// copyright ############################### #
+// This file is part of the Xcoll package.   #
+// Copyright (c) CERN, 2025.                 #
+// ######################################### #
+
+#ifndef XCOLL_BENTCHANNELLINGDEV_H
+#define XCOLL_BENTCHANNELLINGDEV_H
+
+
+#include <math.h>
+#include <stdio.h>
+
+#endif // XCOLL_BENTCHANNELLINGDEV_H
