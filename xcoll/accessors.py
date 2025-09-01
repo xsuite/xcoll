5f8858f97dc357cd488f70b51f533f905a2b6c9f
Frederik F. Van der Veken <frederik@cern.ch>
2025-09-02 00:17:57 +0200

    Made family accessor more logical.

--- xcoll/accessors.py
+++ xcoll/accessors.py
@@ -29,6 +29,7 @@ class XcollAccessor:
 
     def __init__(self, db, names=None, **kwargs_to_set):
         super().__setattr__('_db', db)
+        kwargs_to_set.setdefault('_is_family_sub_accessor', False)
         if names:
             super().__setattr__('names', names)
         elif not 'names' in self.__class__.__dict__:
@@ -71,19 +72,55 @@ class XcollAccessor:
     # def properties(self):
     #     return {attr for ddd in self.values() for attr in ddd.keys()}
 
+    def _get_db_element(self, name):
+        if name in self.names:
+            return self._element_dict[name]
+        else:
+            raise KeyError(f"{self._typename.capitalize()} `{name}` not "
+                           + f"found in {self._dbtype}!")
+
+    def _get_element_attr(self, name, attr, default=None):
+        el = self._get_db_element(name)
+        if isinstance(el, dict):
+            if attr in el:
+                return el[attr]
+            else:
+                return default
+        elif hasattr(el, attr):
+            return getattr(el, attr)
+        else:
+            try:
+                return el.get(attr, default)
+            except:
+                return default
+
+    def _set_element_attr(self, name, attr, value, allow_missing=True):
+        el = self._get_db_element(name)
+        if isinstance(el, dict):
+            if attr in el:
+                el[attr] = value
+            elif not allow_missing:
+                raise AttributeError(f"Attribute `{attr}` not found in "
+                                     + f"{self._typename} `{name}`!")
+        elif hasattr(el, attr):
+            setattr(el, attr, value)
+        else:
+            try:
+                el.set(attr, value)
+            except:
+                if not allow_missing:
+                    raise AttributeError(f"Attribute `{attr}` not found in "
+                                         + f"{self._typename} `{name}`!")
+
+    def _get_element_non_family_attributes(self, name):
+        return self._get_element_attr(name, 'non_family_attributes', default=[])
+
     def __getattr__(self, attr):
         properties = {}
         for name, el in self.items():
-            if hasattr(el, attr):
-                properties[name] = getattr(el, attr)
-            elif isinstance(el, dict):
-                if attr in el:
-                    properties[name] = el[attr]
-            else:
-                try:
-                    properties[name] = el.get(attr)
-                except:
-                    pass
+            prop = self._get_element_attr(name, attr, default='__xc_acc_not_found__')
+            if prop != '__xc_acc_not_found__':
+                properties[name] = prop
         if len(properties) == 0:
             raise AttributeError(f"Attribute `{attr}` not found in {self._dbtype}!")
         # If all values are the same, return a single value
@@ -91,40 +128,42 @@ class XcollAccessor:
         and len({tuple(ii) if isinstance(ii, list) else ii
                 for ii in properties.values()}) == 1:
             return next(iter(properties.values()))
+        # If this is a family accessor, all members should have the attribute
+        if self._is_family_sub_accessor:
+            missing = set(self.names) - set(properties.keys())
+            for name in missing:
+                # It is fine if the attribute is declared non-family
+                if attr not in self._get_element_non_family_attributes(name):
+                    raise AttributeError(f"Attribute `{attr}` is not a family "
+                        + f"attribute, as {self._typename} `{name}` does not "
+                        + f"have it! If this is intentional, add the attribute "
+                        + f"to `{name}`s non_family_attributes list!")
         return properties
 
     def __setattr__(self, attr, value):
-        _ = getattr(self, attr) # Just to check that the attribute exists
+        this_attr = getattr(self, attr) # Just to check that the attribute exists
         if isinstance(value, dict):
-            for el, val in value.items():
-                if el not in self.names:
-                    raise KeyError(f"{self._typename.capitalize()} `{el}` not "
-                                   + f"found in {self._dbtype}!")
-                elif isinstance(self[el], dict):
-                    if attr not in self[el]:
-                        raise AttributeError(f"Attribute `{attr}` not found in "
-                                            + f"{self._typename} `{el}`!")
-                    self[el][attr] = value[el]
-                else:
-                    if not hasattr(self[el], attr):
-                        raise AttributeError(f"Attribute `{attr}` not found in "
-                                            + f"{self._typename} `{el}`!")
-                    setattr(self[el], attr, value[el])
+            if self._is_family_sub_accessor:
+                raise ValueError("Cannot set attribute with a dict on a family!")
+            for name, val in value.items():
+                self._set_element_attr(name, attr, val, allow_missing=False)
         else:
-            # If value is not a dict, we assume it is a single value to set for all elements
-            for name, el in self.items():
-                if isinstance(el, dict):
-                    if attr in el:
-                        el[attr] = value
-                elif hasattr(el, attr):
-                    setattr(el, attr, value)
+            # If the value is not a dict, it is either a family attribute...
+            if self._is_family_sub_accessor:
+                for name, el in self.items():
+                    if attr in self._get_element_non_family_attributes(name):
+                        # This attribute is non-family, so ignore it when setting the family
+                        continue
+                    self._set_element_attr(name, attr, value)
+            # ... or a single value to set for all elements
+            else:
+                for name, el in self.items():
+                    self._set_element_attr(name, attr, value)
 
     def __getitem__(self, name):
-        if name in self.names:
-            return self._element_dict[name]
-        else:
-            raise KeyError(f"{self._typename.capitalize()} `{name}` not "
-                           + f"found in {self._dbtype}!")
+        return self._get_db_element(name)
+
+    # TODO: implement __setitem__ for setting as update (do not delete other attributes)
 
     def __repr__(self):
         return f"<{self.__class__.__name__} at {hex(id(self))} (use .show() " \
@@ -148,35 +187,60 @@ class XcollAccessor:
 class XcollFamilyAccessor(XcollAccessor):
     '''This class can be used as a parent to provide a uniform way to access
     collimators from an underlying database (like a settings dictionary or an
-    xtrack.Line). It works similarily to the parent XcollAccessor class, but
-    additionally it has an attribute '_family_db' that holds the common
-    settings for families of collimators.
-
-    Each collimator can have an attribute 'family' that gives the name of the
-    family it belongs to and an attribute 'overwritten_keys' that lists its
-    attributes that are not taken from the family (even though the latter
-    provides them).
+    xtrack.Line). It works similarily to the parent XcollAccessor class.
+    Additionally, each collimator can have an attribute 'family' that gives
+    the name of the family it belongs to, and 'non_family_attributes' which
+    lists its attributes that will not be updated when the family attribute is
+    set.
     '''
 
     # These are only used for printing and error messages:
     _typename = 'collimator'
 
-    def __init__(self, db, names=None, family_db=None, family_names=None, **kwargs_to_set):
-        kwargs_to_set['_family_db'] = family_db or {}
-        if family_names:
-            kwargs_to_set['family_names'] = family_names
-        elif not 'family_names' in self.__class__.__dict__:
-            kwargs_to_set['family_names'] = list(family_db.keys()) if family_db else []
+    def __init__(self, db, names=None, **kwargs_to_set):
         super().__init__(db=db, names=names, **kwargs_to_set)
+        self._check_family_consistency()
+
+    def _check_family_consistency(self):
+        for name, el in self.items():
+            if hasattr(el, 'family') and not isinstance(el.family, str):
+                raise TypeError(f"{self._typename.capitalize()} `{name}` has "
+                                f"a `family` attribute that is not a string!")
+            if hasattr(el, 'non_family_attributes'):
+                if not hasattr(el, 'family'):
+                    raise AttributeError(f"{self._typename.capitalize()} "
+                        + f"`{name}` has `non_family_attributes` but no "
+                        + f"`family` attribute!")
+                if isinstance(el.non_family_attributes, str):
+                    el.non_family_attributes = [el.non_family_attributes]
+                elif not hasattr(el.non_family_attributes, '__iter__'):
+                    raise TypeError(f"{self._typename.capitalize()} `{name}` "
+                                  + f"has `non_family_attributes` but it is "
+                                  + f"not iterable!")
+                for attr in el.non_family_attributes:
+                    if not (isinstance(el, dict) and attr in el) \
+                    and not hasattr(el, attr):
+                        raise AttributeError(f"{self._typename.capitalize()} "
+                          + f"`{name}` has attribute `{attr}` in "
+                          + f"`non_family_attributes` but it is not present!")
+                    if attr == 'family':
+                        raise ValueError(f"{self._typename.capitalize()} "
+                          + f"`{name}` has `family` in `non_family_attributes`!")
+                    if attr == 'non_family_attributes':
+                        raise ValueError(f"{self._typename.capitalize()} "
+                          + f"`{name}` has `non_family_attributes` in "
+                          + f"`non_family_attributes`!")
 
     def __str__(self):
         res = []
         if len(self.families) > 0:
             res.append('Families:')
-            name_len = max(len(name) for name in self.family_names)
+            name_len = max(max(len(name) for name in self.family_names) + 1, 10)
             for family, names in self.families.items():
                 ff = f'{family}:'
                 res.append(f"    {ff:<{name_len}}  {', '.join(names)}")
+            nofam_names = set(self.names) - {vvv for vv in self.families.values() for vvv in vv}
+            res.append(f"    {'no family:':<{name_len}}  {', '.join(nofam_names)}")
             res.append('')
         res.append(super().__str__())
         return "\n".join(res)
@@ -194,18 +258,18 @@ class XcollFamilyAccessor(XcollAccessor):
                     if prop_families[name] not in families:
                         families[prop_families[name]] = []
                     families[prop_families[name]].append(name)
-                else:
-                    if 'no family' not in families:
-                        families['no family'] = []
-                    families['no family'].append(name)
             return families
+
+    @property
+    def family_names(self):
+        return list(self.families.keys())
 
     def __getitem__(self, name):
         # We can getitem by name or family, so we overwrite the super method
         if name in self.families:
-            return XcollAccessor(db=self._db, names=self.families[name], _typename='collimator',
-                                 _dbtype=self._dbtype)
+            return XcollAccessor(db=self._db, names=self.families[name], _typename=self._typename,
+                                 _dbtype=self._dbtype, _is_family_sub_accessor=True)
         elif name in self.names:
-            return self._db[name]
+            return self._element_dict[name]
         else:
-            raise KeyError(f"Neither family nor collimator `{name}` found in {self._dbtype}!")
+            raise KeyError(f"Neither family nor {self._typename} `{name}` found in {self._dbtype}!")
