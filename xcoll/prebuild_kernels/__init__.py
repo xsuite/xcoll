0bda34b43298b75ee2cacbc7303aa26d68913c1b
Frederik F. Van der Veken <frederik@cern.ch>
2025-09-02 20:37:48 +0200

    Move xcoll elements for prebuild kernels from Xsuite in Xcoll.

---
+++ xcoll/prebuild_kernels/__init__.py
@@ -0,0 +1,7 @@
+# copyright ############################### #
+# This file is part of the Xcoll package.   #
+# Copyright (c) CERN, 2025.                 #
+# ######################################### #
+
+from .elements import DEFAULT_XCOLL_ELEMENTS
+from .element_inits import XCOLL_ELEMENTS_INIT_DEFAULTS
