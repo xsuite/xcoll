98acaec8a50dc7499a9c46fede3386403b3942f9
Frederik F. Van der Veken <frederik@cern.ch>
2025-09-03 23:42:41 +0200

    Fixed bug with sliced Cavity.

--- xcoll/rf_sweep.py
+++ xcoll/rf_sweep.py
@@ -48,7 +48,7 @@ class RFSweep:
             self.sweep_per_turn = sweep / num_turns
         else:
             raise ValueError("Either `sweep` or `sweep_per_turn` must be provided.")
-        self._get_base_frequency(sweep)
+        self._get_cavity_data(self.sweep_per_turn)
         self._install_zeta_shift()
         self.reset() # Initialize rf_sweep_df
         self.env['rf_sweep'].dzeta = f"{self.L} * rf_sweep_df / ({self.f_RF} + rf_sweep_df)"
@@ -58,7 +58,7 @@ class RFSweep:
                 if scale_factor == 1:
                     self.env.ref[cav].frequency += self.env.ref["rf_sweep_df"]
                 else:
-                    self.env.ref[cav].frequency += {scale_factor} * self.env.ref["rf_sweep_df"]
+                    self.env.ref[cav].frequency += scale_factor * self.env.ref["rf_sweep_df"]
         self.line.enable_time_dependent_vars = True
         print("Enabled time-dependent variables in the line.")
 
@@ -70,9 +70,9 @@ class RFSweep:
         t_turn = self.tw.T_rev0
         current_time = self.env['t_turn_s']
         if current_time == 0:
-            self.env['rf_sweep_df'] = f"t_turn_s / {t_turn} * {self.sweep_per_turn}"
+            self.env['rf_sweep_df'] = f"(t_turn_s + {t_turn}) / {t_turn} * {self.sweep_per_turn}"
         else:
-            self.env['rf_sweep_df'] = f"(t_turn_s - {current_time}) / {t_turn} * {self.sweep_per_turn}"
+            self.env['rf_sweep_df'] = f"(t_turn_s - {current_time} + {t_turn}) / {t_turn} * {self.sweep_per_turn}"
 
 
     def info(self, sweep=None, num_turns=None):
@@ -98,7 +98,8 @@ class RFSweep:
         print(f"The current frequency is {self.f_RF + existing_sweep}Hz, adding "
             + f"{self.sweep_per_turn}Hz per turn.")
         print(f"This sweep will move the center of the bucket with \u0394\u03B4 = "
-            + f"{delta_shift} per turn. This implies one bucket shift every {bucket_turns} turns.")
+            + f"{delta_shift} per turn.")
+        print(f"This implies one bucket shift every {bucket_turns} turns.")
         if self.tw.qs * bucket_turns < 3:
             print(f"Warning: This is a very fast sweep, moving ~1 bucket in "
                 + f"~{round(self.tw.qs * bucket_turns, 2)} synchrotron oscillations "
@@ -113,12 +114,12 @@ class RFSweep:
         self.line.track(particles=particles, *args, **kwargs)
 
 
-    def _get_base_frequency(self, sweep):
+    def _get_cavity_data(self, sweep):
+        tt = self.line.get_table()
+        tt_c = tt.rows[[nn for nn, ttt in zip(tt.name, tt.element_type)
+                        if 'Cavity' in ttt and 'CrabCavity' not in ttt]]
+        mask = tt_c.parent_name == None  # Unsliced cavities
         if self.cavities is None:
-            tt = self.line.get_table()
-            tt_c = tt.rows[[nn for nn, ttt in zip(tt.name, tt.element_type)
-                            if 'Cavity' in ttt and 'CrabCavity' not in ttt]]
-            mask = tt_c.parent_name == None  # Unsliced cavities
             self.cavities = np.unique(list(tt_c.name[mask]) + list(tt_c.parent_name[~mask]))
         else:
             if isinstance(self.cavities, str):
@@ -131,6 +132,17 @@ class RFSweep:
         freq = np.array([self.env[cav].frequency for cav in self.cavities])
         volt = np.array([self.env[cav].voltage for cav in self.cavities])
         lag  = np.array([self.env[cav].lag for cav in self.cavities])
+        s_pos = []
+        name_first_in_line = []
+        for cav in self.cavities:
+            if cav in tt_c.name:
+                s_pos.append(tt_c.rows[cav].s_start[0])
+                name_first_in_line.append(cav)
+            elif cav in tt_c.parent_name:
+                s_pos.append(tt_c.rows[tt_c.parent_name == cav].s_start.min())
+                name_first_in_line.append(tt_c.rows[tt_c.parent_name == cav].name[0])
+            else:
+                raise ValueError(f"Cavity `{cav}` not found in the line!")
         idx  = np.argsort(freq)
         boundaries = np.where(~np.isclose(freq[idx][1:], freq[idx][:-1],
                                           rtol=1e-8, atol=1e-12)
@@ -141,7 +153,9 @@ class RFSweep:
                 'names': self.cavities[gidx],
                 'freq': np.mean(freq[gidx]),
                 'voltage': volt[gidx].sum(),
+                's_pos': np.array(s_pos)[gidx],
                 'lag': lag[gidx],
+                'name_first_in_line': np.array(name_first_in_line)[gidx],
             })
         self.cavities = sorted(groups, key=lambda g: (g['voltage'], g['freq']), reverse=True)
         if np.isclose(self.cavities[0]['voltage'], 0):
@@ -154,14 +168,15 @@ class RFSweep:
             print("Found multiple cavities with different frequencies:")
             for g in self.cavities:
                 print(f"{g['freq']}Hz  at {g['voltage']}V: {g['names']}")
-            print(r"The sweep ($\Delta f$ = " + f"{sweep}Hz) will be performed "
-                + f"with respect to the highest voltage cavity at {self.f_RF}Hz"
-                + f". The other cavities will be shifted accordingly.")
+            print(r"The sweep ($\Delta f$ = " + f"{sweep}Hz per turn) will be "
+                + f"performed with respect to the highest voltage cavity at "
+                + f"{self.f_RF}Hz. The other cavities will be shifted "
+                + f"accordingly.")
 
 
     def _install_zeta_shift(self):
         if 'rf_sweep' not in self.env.elements:
-            idx  = np.argsort([self.line.get_s_position(cav) for cav in self.cavities[0]['names']])
-            first_cavity = self.cavities[0]['names'][idx[0]]
+            idx = np.argsort(self.cavities[0]['s_pos'])
+            first_cavity = self.cavities[0]['name_first_in_line'][idx[0]]
             self.env.elements['rf_sweep'] = xt.ZetaShift(dzeta=0)
             self.line.insert('rf_sweep', at=f'{first_cavity}@start')
