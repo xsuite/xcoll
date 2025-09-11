2b36719e9c696755205eb0688fb05d81d40c1f5b
Frederik F. Van der Veken <frederik@cern.ch>
2025-09-11 17:54:48 +0200

    Made RFSweep backwards compatible (though with a warning).

--- tests/test_rf_sweep.py
+++ tests/test_rf_sweep.py
@@ -20,27 +20,52 @@ def test_rf_sweep(sweep, beam, test_context):
     num_turns = 6000
     num_particles = 5
     if beam == 3:
+        bh = 3.5e-3
+        sweep *= 25
         line = xt.load(path / f'sequence_sps_q20_inj.json')['sps']
         line.insert('markkk', xt.Marker(), at=2822)
         tt_c = line.get_table().rows['ac.*']
         assert 'ThickSliceCavity' in tt_c.element_type
     else:
+        bh = 3.e-4
         line = xt.load(path / f'sequence_lhc_run3_b{beam}.json')
+
+    line.build_tracker(_context=test_context)
+
+    part = line.build_particles(delta=np.linspace(-0.7*bh, 0.7*bh, num_particles),
+                                x_norm=0, px_norm=0, y_norm=0, py_norm=0, _context=test_context)
+
+    rf_sweep = xc.RFSweep(line)
+    rf_sweep.prepare(sweep_per_turn=sweep/num_turns)
+    rf_sweep.info()
+    line.track(particles=part, num_turns=num_turns)
+
+    # This sweep is 3.5 buckets, so check that all particles are at least 3 buckets away
+    # negative sweep => positive off-momentum etc
+    if sweep < 0:
+        assert np.all(part.delta > 3*bh)
+    else:
+        assert np.all(part.delta < -3*bh)
+
+
+def test_rf_sweep_old_style():
+    num_turns = 6000
+    num_particles = 5
+    line = xt.load(path / f'sequence_lhc_run3_b2.json')
 
     line.build_tracker()
 
     part = line.build_particles(delta=np.linspace(-2e-4, 2e-4, num_particles),
                                 x_norm=0, px_norm=0, y_norm=0, py_norm=0)
 
+    sweep = -300
     rf_sweep = xc.RFSweep(line)
-    rf_sweep.prepare(sweep_per_turn=sweep/num_turns)
-    rf_sweep.info()
+    with pytest.warns(DeprecationWarning, match="The `sweep` argument is deprecated."):
+        with pytest.warns(DeprecationWarning, match="The `num_turns` argument is deprecated."):
+            rf_sweep.info(sweep=sweep, num_turns=num_turns)
+    with pytest.warns(DeprecationWarning):
+        rf_sweep.track(sweep=sweep, num_turns=num_turns, particles=part)
+
     # This sweep is 3.5 buckets, so check that all particles are at least 3 buckets away
-    line.track(particles=part, num_turns=num_turns)
-
     # negative sweep => positive off-momentum etc
-    if sweep < 0:
-        assert np.all(part.delta > 1.5e-3)
-    else:
-        assert np.all(part.delta < -1.5e-3)
-
+    assert np.all(part.delta > 1.5e-3)
--- xcoll/rf_sweep.py
+++ xcoll/rf_sweep.py
@@ -4,12 +4,13 @@
 # ######################################### #
 
 import numpy as np
+from warnings import warn
 import scipy.constants as sc
 
 import xtrack as xt
 
 
-def prepare_rf_sweep(line, cavities=None, sweep=None, sweep_per_turn=None, num_turns=None):
+def prepare_rf_sweep(line, *, cavities=None, sweep=None, sweep_per_turn=None, num_turns=None):
     rf_sweep = RFSweep(line=line, cavities=cavities)
     rf_sweep.prepare(sweep=sweep, sweep_per_turn=sweep_per_turn, num_turns=num_turns)
 
@@ -21,53 +22,59 @@ class RFSweep:
             raise ValueError("Line must have a `var_management` to use RFSweep!" \
                            + "Do not use `optimize_for_tracking` as it will "
                            + "disable expressions, which are needed for RFSweep.")
+        line.enable_time_dependent_vars = False  # To be able to twiss in the prepare step
         self.env = line.env
         self.cavities = cavities
         self.env['rf_sweep_df'] = 0
+        self._prepared = False
 
     @property
     def current_sweep_value(self):
         return self.env['rf_sweep_df']
 
-    def prepare(self, sweep=None, sweep_per_turn=None, num_turns=None):
-        if sweep_per_turn is not None:
-            if np.isclose(sweep_per_turn, 0):
-                raise ValueError("Variable `sweep_per_turn` must be non-zero!")
-            if sweep is not None:
-                raise ValueError("Provide either `sweep` or `sweep_per_turn`, not both.")
-            if num_turns is not None:
-                raise ValueError("Variable `num_turns` cannot be set when using `sweep_per_turn`.")
-            self.sweep_per_turn = sweep_per_turn
-        elif sweep is not None:
-            if np.isclose(sweep, 0):
-                raise ValueError("Variable `sweep` must be non-zero!")
-            if num_turns is None:
-                num_turns = int(abs(sweep))
-            if num_turns <= 0:
-                raise ValueError("When using `sweep`, `num_turns` must be a positive integer.")
-            self.sweep_per_turn = sweep / num_turns
-        else:
-            raise ValueError("Either `sweep` or `sweep_per_turn` must be provided.")
-        self._get_cavity_data(self.sweep_per_turn)
-        self._install_zeta_shift()
-        self.tw = self.line.twiss()
-        self.reset() # Initialize rf_sweep_df
-        self.env['rf_sweep'].dzeta = f"{self.L} * rf_sweep_df / ({self.f_RF} + rf_sweep_df)"
-        for cavs in self.cavities:
-            for cav in cavs['names']:
-                scale_factor = int(np.round(cavs['freq'] / self.f_RF))
-                if scale_factor == 1:
-                    self.env.ref[cav].frequency += self.env.ref["rf_sweep_df"]
-                else:
-                    self.env.ref[cav].frequency += scale_factor * self.env.ref["rf_sweep_df"]
-        self.line.enable_time_dependent_vars = True
-        print("Enabled time-dependent variables in the line.")
+    def prepare(self, *, sweep=None, num_turns=None, sweep_per_turn=None):
+        # TODO: if using sweep and num_turns, the sweep should stop after num_turns,
+        #       but if using sweep_per_turn, it should continue indefinitely.
+        if not self._prepared:
+            if sweep_per_turn is not None:
+                if np.isclose(sweep_per_turn, 0):
+                    raise ValueError("Variable `sweep_per_turn` must be non-zero!")
+                if sweep is not None:
+                    raise ValueError("Provide either `sweep` or `sweep_per_turn`, not both.")
+                if num_turns is not None:
+                    raise ValueError("Variable `num_turns` cannot be set when using `sweep_per_turn`.")
+                self.sweep_per_turn = sweep_per_turn
+            elif sweep is not None:
+                if np.isclose(sweep, 0):
+                    raise ValueError("Variable `sweep` must be non-zero!")
+                if num_turns is None:
+                    num_turns = int(abs(sweep))
+                if num_turns <= 0:
+                    raise ValueError("When using `sweep`, `num_turns` must be a positive integer.")
+                self.sweep_per_turn = sweep / num_turns
+            else:
+                raise ValueError("Either `sweep` or `sweep_per_turn` must be provided.")
+            self._get_cavity_data()
+            self._install_zeta_shift()
+            self.tw = self.line.twiss()
+            self.reset() # Initialize rf_sweep_df
+            self.env['rf_sweep'].dzeta = f"{self.L} * rf_sweep_df / ({self.f_RF} + rf_sweep_df)"
+            for cavs in self.cavities:
+                for cav in cavs['names']:
+                    scale_factor = int(np.round(cavs['freq'] / self.f_RF))
+                    if scale_factor == 1:
+                        self.env.ref[cav].frequency += self.env.ref["rf_sweep_df"]
+                    else:
+                        self.env.ref[cav].frequency += scale_factor * self.env.ref["rf_sweep_df"]
+            self.line.enable_time_dependent_vars = True
+            print("Enabled time-dependent variables in the line.")
+            self._prepared = True
 
 
     def reset(self):
         if self.sweep_per_turn is None:
             raise ValueError("RFSweep not prepared. Call `prepare` first.")
-        t_turn = self.tw.T_rev0
+        t_turn = self.tw.T_rev0   # To start sweeping from turn 0
         current_time = self.env['t_turn_s']
         if current_time == 0:
             self.env['rf_sweep_df'] = f"(t_turn_s + {t_turn}) / {t_turn} * {self.sweep_per_turn}"
@@ -76,10 +83,12 @@ class RFSweep:
 
 
     def info(self, sweep=None, num_turns=None):
-        if sweep is not None:
-            raise DeprecationWarning("The `sweep` argument is deprecated.")
-        if num_turns is not None:
-            raise DeprecationWarning("The `num_turns` argument is deprecated.")
+        if sweep is not None or num_turns is not None:
+            if sweep is not None:
+                warn("The `sweep` argument is deprecated.", DeprecationWarning)
+            if num_turns is not None:
+                warn("The `num_turns` argument is deprecated.", DeprecationWarning)
+            self.prepare(sweep=sweep, num_turns=num_turns)
         if self.sweep_per_turn is None:
             raise ValueError("RFSweep not prepared. Call `prepare` first.")
         existing_sweep = self.current_sweep_value
@@ -98,8 +107,9 @@ class RFSweep:
         print(f"The current frequency is {self.f_RF + existing_sweep}Hz, adding "
             + f"{self.sweep_per_turn}Hz per turn.")
         print(f"This sweep will move the center of the bucket with \u0394\u03B4 = "
-            + f"{delta_shift} per turn.")
-        print(f"This implies one bucket shift every {bucket_turns} turns.")
+            + f"{delta_shift:.4} per turn.")
+        print(f"The bucket height is {bucket_height:.4}, so this implies the sweep "
+            + f"will shift one bucket every {round(bucket_turns, 2)} turns.")
         if self.tw.qs * bucket_turns < 3:
             print(f"Warning: This is a very fast sweep, moving ~1 bucket in "
                 + f"~{round(self.tw.qs * bucket_turns, 2)} synchrotron oscillations "
@@ -110,11 +120,14 @@ class RFSweep:
 
     # For backward compatibility
     def track(self, sweep=0, particles=None, num_turns=0, verbose=True, *args, **kwargs):
-        self.prepare(sweep, num_turns)
-        self.line.track(particles=particles, *args, **kwargs)
+        warn("Using RFSweep.track() is deprecated. Please use RFSweep.prepare() "
+           + "and then xt.Line.track().", DeprecationWarning)
+        self.prepare(sweep=sweep, num_turns=num_turns)
+        self.line.track(particles=particles, num_turns=num_turns, *args, **kwargs)
 
 
-    def _get_cavity_data(self, sweep):
+    def _get_cavity_data(self):
+        # Cavity data not yet extracted
         tt = self.line.get_table()
         tt_c = tt.rows[[nn for nn, ttt in zip(tt.name, tt.element_type)
                         if 'Cavity' in ttt and 'CrabCavity' not in ttt]]
@@ -145,8 +158,8 @@ class RFSweep:
                 raise ValueError(f"Cavity `{cav}` not found in the line!")
         idx  = np.argsort(freq)
         boundaries = np.where(~np.isclose(freq[idx][1:], freq[idx][:-1],
-                                          rtol=1e-8, atol=1e-12)
-                              )[0] + 1
+                                        rtol=1e-8, atol=1e-12)
+                            )[0] + 1
         groups = []
         for gidx in np.split(idx, boundaries):
             groups.append({
@@ -168,10 +181,9 @@ class RFSweep:
             print("Found multiple cavities with different frequencies:")
             for g in self.cavities:
                 print(f"{g['freq']}Hz  at {g['voltage']}V: {g['names']}")
-            print(r"The sweep ($\Delta f$ = " + f"{sweep}Hz per turn) will be "
-                + f"performed with respect to the highest voltage cavity at "
-                + f"{self.f_RF}Hz. The other cavities will be shifted "
-                + f"accordingly.")
+            print(r"The sweep will be performed with respect to the highest "
+                + f"voltage cavity at {self.f_RF}Hz. The other cavities will "
+                + f"be shifted accordingly.")
 
 
     def _install_zeta_shift(self):
