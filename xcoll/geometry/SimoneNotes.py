ffac0810089c8fd8f458939f1c8127abb917bc22
simoneotelie <simoneos@stud.ntnu.no>
2025-10-13 20:51:46 +0200

    Added temporary file for notes during meetings

---
+++ xcoll/geometry/SimoneNotes.py
@@ -0,0 +1,10 @@
+This will be deleted later
+
+Todo: 
+* Need to fix the slicing box. Deadlock + some bug
+* Connect with Everest & Nuclear interactions. Figure out how to connect path length to scattering through probability
+* test test test
+
+Small todo:
+* Possibly move the crossings folder to scattering part of Xcoll
+* Rename FindRoot to something thats more generic and explains both crossings AND path length 
