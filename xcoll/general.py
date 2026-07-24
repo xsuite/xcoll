9eb7ad769999e98b455b23be7157b3af3af2779a
Frederik F. Van der Veken <frederik@cern.ch>
2026-07-24 11:30:54 +0200

    Created release branch release/v0.12.1rc0.

--- xcoll/general.py
+++ xcoll/general.py
@@ -15,5 +15,5 @@ citation = "F.F. Van der Veken, et al.: Recent Developments with the New Tools f
 # ======================
 # Do not change
 # ======================
-__version__ = '0.12.0'
+__version__ = '0.12.1rc0'
 # ======================
