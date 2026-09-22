d541510754b05a2cfe3ac4f641adf3abb7dc0925
Frederik F. Van der Veken <frederik@cern.ch>
2026-09-22 18:24:55 +0200

    Improved ion test

--- tests/test_ions.py
+++ tests/test_ions.py
@@ -12,6 +12,7 @@ import xtrack as xt
 import xpart as xp
 import xcoll as xc
 import xtrack.particles.pdg as pdg
+import xtrack.particles.masses as xpm
 from  xcoll import constants as xcc
 
 from xobjects.test_helpers import for_all_test_contexts
@@ -25,43 +26,74 @@ particle_ref = xt.Particles('Pu-239', p0c=94*7.0e12)
 
 @pytest.mark.parametrize("engine", engine_params)
 def test_ions(engine):
-    num_part = 500
-    length = 0.01
+    num_part = 2000
+    capacity = 100*num_part
+    length = 0.05
     jaw = 0.001
     material = 'Ti'
     seed = 1336
 
     if engine == "fluka":
-        if xc.fluka.engine.is_running():
-            xc.fluka.engine.stop(clean=True)
+        xc_engine = xc.fluka.engine
+        xc_engine.relative_length_fortran_array = 100
         coll = xc.FlukaCollimator(length=length, jaw=jaw, material=material)
-        xc.fluka.engine.particle_ref = particle_ref
-        xc.fluka.engine.start(elements=coll, seed=seed,
-                              relative_length_fortran_array=10)
-        part = xp.build_particles(x=np.ones(num_part)*2*coll.jaw_L,
-                                  particle_ref=xc.fluka.engine.particle_ref,
-                                  _capacity=10*num_part)
+
     elif engine == "geant4":
-        if xc.geant4.engine.is_running():
-            xc.geant4.engine.stop(clean=True)
+        xc_engine = xc.geant4.engine
         coll = xc.Geant4Collimator(length=length, jaw=jaw, material=material)
-        xc.geant4.engine.particle_ref = particle_ref
-        xc.geant4.engine.start(elements=coll, seed=seed)
-        part = xp.build_particles(x=np.ones(num_part)*2*coll.jaw_L,
-                                 particle_ref=xc.geant4.engine.particle_ref,
-                                 _capacity=10*num_part)
+
+    if xc_engine.is_running():
+        xc_engine.stop(clean=True)
+
+    xc_engine.particle_ref = particle_ref
+    xc_engine.return_none = True
+    xc_engine.return_ions = True
+    xc_engine.start(elements=coll, seed=seed)
+    part = xp.build_particles(x=np.ones(num_part)*2*coll.jaw_L,
+                                particle_ref=xc_engine.particle_ref,
+                                _capacity=capacity)
 
     t_start = time.time()
     coll.track(part)
+    xc_engine.stop(clean=True)
+
     print(f"Time per track: {(time.time()-t_start)*1e3:.2f}ms for "
         + f"{num_part} Pu-239 ions through {coll.length:.2f}m")
 
-    if engine == "fluka":
-        xc.fluka.engine.stop(clean=True)
-    elif engine == "geant4":
-        xc.geant4.engine.stop(clean=True)
+    mask_children = part.particle_id >= num_part
+    pdg_ids = part.pdg_id[mask_children]
+    print(f"Generated {len(pdg_ids)} children.")
 
-    # Get only the initial particles that survived and all new particles (even if dead, as neutral particles will be flagged dead)
+    # We explicitly requested ions only
+    assert len(pdg_ids) > 0
+    assert np.all(pdg_ids > 1_000_000_000)
+    _, A, Z, _ = pdg.get_properties_from_pdg_id(pdg_ids)
+    assert np.all(A >= 2)
+    assert np.all(Z >= 1)
+    assert np.all(A >= Z)
+
+    # Check masses
+    A_from_mass = np.rint(part.mass[mask_children] / xpm.U_MASS_EV).astype(int)
+    assert np.array_equal(A_from_mass, A)
+
+    # Require a broad fragmentation spectrum
+    assert len(np.unique(A)) > 100
+    assert len(np.unique(Z)) > 40
+
+    A_core = np.unique(A[(A >= 20) & (A < 100)])
+    coverage = len(A_core) / 80
+    print(f"A coverage in [20, 100): {coverage:.1%}")
+    assert coverage > 0.9  # FLUKA 100% Geant4 95%
+    assert np.max(np.diff(A_core)) <= 4
+    assert set(A_core % 10) == set(range(10)) # Test that the last digit of A is not only 0 (was a bug)
+
+    # Check iostopes coverage in [100, 240[
+    A_tail = np.unique(A[(A >= 100) & (A < 240)])
+    coverage = len(A_tail) / (np.max(A_tail) - 100 + 1) if len(A_tail) > 0 else 0
+    print(f"A coverage in [100, {np.max(A_tail) if len(A_tail) > 0 else 100}): {coverage:.1%}")
+    assert coverage > 0.5  # FLUKA 63% Geant4 64%
+
+    # print info
     mask = (part.state > 0) | (part.particle_id >= num_part)
     pdg_ids, counts = np.unique(part.pdg_id[mask], return_counts=True)
     for pdg_id, num in zip(pdg_ids, counts):
@@ -76,6 +108,3 @@ def test_ions(engine):
         E = part.energy[mask & (part.pdg_id==pdg_id)]
         en = f"{E[~np.isnan(E)].mean():.1e} ± {E[~np.isnan(E)].std():.1e} eV"
         print(f"  {num:6} {name:12}{en:21}  (PDG ID: {pdg_id}, mass: {mass} eV)")
-
-    # Check that we have some fission products (PDG ID > 1 billion
-    assert np.sum((pdg_ids > 1000000000) & (pdg_ids < 1000942390)) > 0
